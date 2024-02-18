3e07565c67d4b50cb097f04b997ed338d1da1dd0
Talha <mdtalha2008@gmail.com>
2024-02-18 16:32:02 +0600

    lindin add

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { GrFacebookOption } from 'react-icons/gr';
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 const Footer = () => {
   return (
@@ -45,38 +47,17 @@ const Footer = () => {
       <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
         <nav>
           <div className="grid grid-flow-col gap-4">
-          <Link>
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://www.facebook.com/talha.tarique.5"
-                  className=" "
-                >
-                  <GrFacebookOption size="1.7rem" color="black" />
-                </a>
-              </Link>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
-            </a>
+          <Link to="">
+          <a href=""><FaFacebookF size="1.7rem" color="white" /></a>
+          </Link>
+          <Link to="">
+          <a href=""><FaInstagram size="1.7rem" color="white" /></a>
+          </Link>
+          <Link to="">
+          <a href=""><FaLinkedinIn size="1.7rem" color="white" /></a>
+          </Link>
+           
+            
           </div>
         </nav>
         <aside>
