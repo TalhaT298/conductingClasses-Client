43783a648d2465e684c6bfa0db72f21f874154c4
Talha <mdtalha2008@gmail.com>
2024-02-22 01:44:22 +0600

    testimonal add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Photos from '../../Photos/Photos';
+import Testimonial from '../Testimonial/Testimonial';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Testimonial></Testimonial>
             <Photos></Photos>
         </div>
     );
