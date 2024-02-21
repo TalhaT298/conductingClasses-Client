bd10bc9bdd9203585f2f2d6c3cbc1b37bd170777
Talha <mdtalha2008@gmail.com>
2024-02-22 01:05:27 +0600

    slide 8 add

--- src/pages/Photos/Photos.jsx
+++ src/pages/Photos/Photos.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import slide1 from '../../assets/78.webp'
-import slide2 from '../../assets/77.webp'
-import slide3 from '../../assets/79.webp'
+import slide1 from '../../assets/78.webp';
+import slide2 from '../../assets/77.webp';
+import slide3 from '../../assets/79.webp';
+import slide4 from '../../assets/80.webp';
+import slide5 from '../../assets/81.webp';
+import slide6 from '../../assets/82.webp';
+import slide7 from '../../assets/83.webp';
+import slide8 from '../../assets/84.webp';
 const Photos = () => {
     return (
         <div>
@@ -11,13 +16,12 @@ const Photos = () => {
         <SwiperSlide><img src={slide1} alt="" /></SwiperSlide>
         <SwiperSlide><img src={slide2} alt="" /></SwiperSlide>
         <SwiperSlide><img src={slide3} alt="" /></SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide><img src={slide4} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide5} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide6} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide7} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide8} alt="" /></SwiperSlide>
+        
       </Swiper>
         </div>
     );
