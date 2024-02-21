844fbb0cb25d963fbbd8af7c1934a90901b921c7
Talha <mdtalha2008@gmail.com>
2024-02-21 22:56:33 +0600

    footer add

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -10,7 +10,7 @@ import { FaDiscord } from "react-icons/fa";
 import { FaThreads } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
@@ -18,105 +18,192 @@ const Footer = () => {
       <footer className="footer p-10 bg-base-200 text-base-content">
         <nav>
           <h6 className="footer-title">Get in Touch</h6>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Contact</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Contact
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Meet With us</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Meet With us
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Privacy Statement</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Privacy Statement
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Report Copyright Infringement</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Report Copyright Infringement
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Report on Security Issues</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Report on Security Issues
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Recom. For Traffic Mgt</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Recom. For Traffic Mgt
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Newsletters</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Newsletters
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Location Map</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Location Map
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Covid-19 updates</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Covid-19 updates
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Logos (Daffodil Family)</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Logos (Daffodil Family)
+            </a>
           </Link>
-          
         </nav>
         <nav>
-        <h6 className="footer-title">Branding</h6>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>DIU Branding</a>
+          <h6 className="footer-title">Branding</h6>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              DIU Branding
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Career Opportunities</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Career Opportunities
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Blog</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Blog
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Photo Gallery</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Photo Gallery
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>DIU News</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              DIU News
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>DIU Press</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              DIU Press
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Employability 360</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Employability 360
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>DIU IT</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              DIU IT
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Artofliving</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Artofliving
+            </a>
           </Link>
-          
         </nav>
         <nav>
           <h6 className="footer-title">Useful Links</h6>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>skill.jobs</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              skill.jobs
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Internship Portal</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Internship Portal
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Foundation Day</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Foundation Day
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Convocation</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Convocation
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>DIU Annual Report</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              DIU Annual Report
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Brochure</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Brochure
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Prospectus</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Prospectus
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Forms</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Forms
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Brand Documents</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Brand Documents
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>Apps</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              Apps
+            </a>
           </Link>
-          <Link to=''>
-          <a className="link link-hover flex"><PiArrowRightLight size="1.3rem" color="white"/>FAQ</a>
+          <Link to="">
+            <a className="link link-hover flex">
+              <PiArrowRightLight size="1.3rem" color="white" />
+              FAQ
+            </a>
           </Link>
-          
         </nav>
         <form>
           <h6 className="footer-title">Subscribe Us!</h6>
@@ -138,49 +225,72 @@ const Footer = () => {
       <footer className="footer footer-center p-8 bg-base-200 text-base-content rounded">
         <nav>
           <div className="grid grid-flow-col gap-3">
-          <motion.div
-          whileTap={{ rotate: 90, scale: 0.75 }}
-          >
-          <Link to="">
-          <a href=""><FaFacebookF size="1.7rem" color="white" /></a>
-          </Link>
-          </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaFacebookF size="1.7rem" color="white" />
+                </a>
+              </Link>
+            </motion.div>
 
-          <motion.div
-          whileTap={{ rotate: 90, scale: 0.75 }}
-          >
-          <Link to="">
-          <a href=""><FaInstagram size="1.7rem" color="white" /></a>
-          </Link>
-          </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaInstagram size="1.7rem" color="white" />
+                </a>
+              </Link>
+            </motion.div>
 
-          <motion.div
-          whileTap={{ rotate: 90, scale: 0.75 }}
-          >
-          <Link to="">
-          <a href=""><FaLinkedinIn size="1.7rem" color="white" /></a>
-          </Link>
-          </motion.div>
-          <Link to="">
-          <a href=""><BsTwitterX size="1.7rem" color="white" /></a>
-          </Link>
-          <Link to="">
-          <a href=""><FaWhatsapp size="1.7rem" color="white" /></a>
-          </Link>
-          <Link to="">
-          <a href=""><FaDiscord size="1.7rem" color="white" /></a>
-          </Link>
-          <Link to="">
-          <a href=""><FaThreads size="1.7rem" color="white" /></a>
-          </Link>
-          <Link to="">
-          <a href=""><FaYoutube size="1.7rem" color="white" /></a>
-          </Link>
-          <Link to="">
-          <a href=""><BiLogoGmail size="1.7rem" color="white" /></a>
-          </Link>
-           
-            
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaLinkedinIn size="1.7rem" color="white" />
+                </a>
+              </Link>
+            </motion.div>
+
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <BsTwitterX size="1.7rem" color="white" />
+                </a>
+              </Link>
+            </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaWhatsapp size="1.7rem" color="white" />
+                </a>
+              </Link>
+            </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaDiscord size="1.7rem" color="white" />
+                </a>
+              </Link>
+            </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaThreads size="1.7rem" color="white" />
+                </a>
+              </Link>
+            </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaYoutube size="1.7rem" color="white" />
+                </a>
+              </Link>
+            </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <BiLogoGmail size="1.7rem" color="white" />
+                </a>
+              </Link>
+            </motion.div>
           </div>
         </nav>
         <aside>
