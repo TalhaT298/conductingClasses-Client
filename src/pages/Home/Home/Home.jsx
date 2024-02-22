81777295006f6b0bbe9a1693e3688b676ef28df4
Talha <mdtalha2008@gmail.com>
2024-02-22 22:54:25 +0600

    Faculties add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Photos from '../../Photos/Photos';
 import Testimonial from '../Testimonial/Testimonial';
+import Faculties from '../Faculties/Faculties';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Faculties></Faculties>
             <Testimonial></Testimonial>
             <Photos></Photos>
         </div>
