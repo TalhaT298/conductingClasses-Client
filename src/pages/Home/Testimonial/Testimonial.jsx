1c6f760f094428378b9119be3997f50f930657ea
Talha <mdtalha2008@gmail.com>
2024-02-22 13:39:44 +0600

    navigation add

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
 const Testimonial = () => {
     return (
         <section>
