71bdf86b6abdb475f94345cca11bfb59d2699774
Talha <mdtalha2008@gmail.com>
2024-02-22 22:59:30 +0600

    recent event add

--- src/pages/Home/RecentEvents/RecentEvents.jsx
+++ src/pages/Home/RecentEvents/RecentEvents.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const RecentEvents = () => {
     return (
         <div>
-            
+            <SectionTitle
+            heading={"Recent Events"}
+            // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
+            >
+            </SectionTitle>
+            <div>
+                <p>This is recent event</p>
+            </div>
         </div>
     );
 };
