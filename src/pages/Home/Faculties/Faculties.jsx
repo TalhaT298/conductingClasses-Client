2102f6c8d72900b1c9dbc1ebdfec04cbba6ea2ed
Talha <mdtalha2008@gmail.com>
2024-02-22 22:53:32 +0600

    faculties title add

--- src/pages/Home/Faculties/Faculties.jsx
+++ src/pages/Home/Faculties/Faculties.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const Faculties = () => {
     return (
         <div>
-            
+            <SectionTitle
+            heading={"Faculties"}
+            // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
+            >
+            </SectionTitle>
         </div>
     );
 };
