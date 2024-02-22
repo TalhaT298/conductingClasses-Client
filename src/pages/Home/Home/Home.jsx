85da0d12f031f456c685c9fcb1e675e560f561cf
Talha <mdtalha2008@gmail.com>
2024-02-22 23:02:30 +0600

    recent event add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,12 +3,14 @@ import Banner from '../Banner/Banner';
 import Photos from '../../Photos/Photos';
 import Testimonial from '../Testimonial/Testimonial';
 import Faculties from '../Faculties/Faculties';
+import RecentEvents from '../RecentEvents/RecentEvents';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Faculties></Faculties>
+            <RecentEvents></RecentEvents>
             <Testimonial></Testimonial>
             <Photos></Photos>
         </div>
