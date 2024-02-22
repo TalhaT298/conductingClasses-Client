6c4c42d269abb469481a4b2e0d8ab5af3fbe85b2
Talha <mdtalha2008@gmail.com>
2024-02-22 18:33:14 +0600

    visitors update

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -8,7 +8,7 @@ import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 import { Rating } from "@smastrom/react-rating";
 import '@smastrom/react-rating/style.css'
-import { TfiThought } from "react-icons/tfi";
+// import { TfiThought } from "react-icons/tfi";
 
 
 const Testimonial = () => {
@@ -19,23 +19,23 @@ const Testimonial = () => {
       .then((data) => setReviews(data));
   }, []);
     return (
-        <section className='my-20'>
+        <section className='my-10'>
             <SectionTitle
             
             heading={"Visitors Says About Us"}
             // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
-            
             >
             </SectionTitle>
-            
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         {reviews.map((review) => (
           <SwiperSlide color="white" key={review._id}>
-            <div className="flex flex-col items-center mx-24 my-16">
+            <div className="flex flex-col items-center mx-24 my-8">
             <div className="pb-20">
-            <TfiThought className="" size='4.7rem' color='white'></TfiThought>
+            {/* <TfiThought className="" size='4.7rem' color='white'></TfiThought> */}
+            
             </div>
-              <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+              {/* <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly /> */}
+              <img className='w-40 rounded-full' src={review.img} alt={review.name} />
               <p className="py-8">{review.details}</p>
               <h3 className="text-2xl text-orange-400">{review.name}</h3>
             </div>
