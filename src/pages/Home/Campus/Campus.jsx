489cba528c164b9c062f63bb2177a001c1c399d7
Talha <mdtalha2008@gmail.com>
2024-02-24 12:53:03 +0600

    section title add at campus

--- src/pages/Home/Campus/Campus.jsx
+++ src/pages/Home/Campus/Campus.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const Campus = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="featured-item bg-fixed text-white mb-12 pt-6 my-20">
+      <SectionTitle
+        subHeading={"check it out"}
+        heading={"Feature"}
+      ></SectionTitle>
+      <div className="md:flex justify-center items-center py-20 pt-12 px-36">
+      <div>
+        <img src={feature} alt="" />
+      </div>
+      <div className="md:ml-10">
+        <p>Aug 20,2024</p>
+        <p className="uppercase">Where I can get Some</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        <button className="btn btn-outline border-0 border-b-4 text-white">Read More</button>
+      </div>
+      </div>
+    </section>
     );
 };
 
