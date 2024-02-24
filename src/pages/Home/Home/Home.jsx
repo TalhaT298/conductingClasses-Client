f345bca88315afd3e9d9cfeaeaf6f62f6f6a6e9d
Talha <mdtalha2008@gmail.com>
2024-02-24 11:38:15 +0600

    marquee add

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import Photos from '../../Photos/Photos';
-import Testimonial from '../Testimonial/Testimonial';
-import Faculties from '../Faculties/Faculties';
-import RecentEvents from '../RecentEvents/RecentEvents';
+import React from "react";
+import Banner from "../Banner/Banner";
+import Photos from "../../Photos/Photos";
+import Testimonial from "../Testimonial/Testimonial";
+import Faculties from "../Faculties/Faculties";
+import RecentEvents from "../RecentEvents/RecentEvents";
+import Mark from "../Mark/Mark";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <Faculties></Faculties>
-            <RecentEvents></RecentEvents>
-            <Testimonial></Testimonial>
-            <Photos></Photos>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <Faculties></Faculties>
+      <RecentEvents></RecentEvents>
+      <Mark></Mark>
+      <Testimonial></Testimonial>
+      <Photos></Photos>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/Home/Mark/Mark.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import img1 from "../../../assets/tailwind.png"
+import img2 from "../../../assets/visual.png"
+import img3 from "../../../assets/vite.png"
+import img4 from "../../../assets/yarn.png"
+
+const Mark = () => {
+    return (
+        <div>
+            <Marquee>
+                <img src={img1} alt="" />
+                <img src={img2} alt="" />
+                <img src={img3} alt="" />
+                <img src={img4} alt="" />
+            </Marquee>
+        </div>
+    );
+};
+
+export default Mark;
