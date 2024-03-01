406e10e2285a58eeb53d4618203c99e47f69a8da
Talha <mdtalha2008@gmail.com>
2024-03-02 02:52:06 +0600

    read more add

--- src/pages/Home/Notice/Notice.jsx
+++ src/pages/Home/Notice/Notice.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const Notice = () => {
-    return (
-        <div>
-            <div className='w-1/2'> 
-                <div></div>
-                <div className='flex'>
-                    <button><span>18</span> <br />Feb 2024</button>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, sed.</p>
-                    <p>read more</p>
-                </div>
-            </div>
-            <div className='w-1/2'></div>
+  return (
+    <div>
+      <div className="w-1/2">
+        <div></div>
+
+        <div className="">
+        <div class="flex ">
+          <button class="bg-gray-200 p-2 rounded w-20 h-20">
+            <span class="text-3xl">18</span> <br />
+            <span class="text-xs">Feb 2024</span>
+          </button>
+          <p class="pl-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+            sed.hjjbbjbjbjbj
+          </p>
         </div>
-    );
+        <p className="pl-[480px]">read more</p>
+        </div>
+
+      </div>
+      <div className="w-1/2"></div>
+    </div>
+  );
 };
 
 export default Notice;
