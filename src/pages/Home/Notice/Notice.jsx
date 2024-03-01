a342145603693a4d1d8f898f57b6d87c9cfa7696
Talha <mdtalha2008@gmail.com>
2024-03-02 02:20:32 +0600

    div add

--- src/pages/Home/Notice/Notice.jsx
+++ src/pages/Home/Notice/Notice.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Notice = () => {
     return (
         <div>
-            
+            <div className='w-1/2'> 
+                <div></div>
+                <div>
+
+                </div>
+            </div>
+            <div className='w-1/2'></div>
         </div>
     );
 };
