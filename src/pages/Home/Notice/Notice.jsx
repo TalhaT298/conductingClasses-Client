e7f6b357b67c86226ce820ec54d59574b17eb5d0
Talha <mdtalha2008@gmail.com>
2024-03-02 03:14:46 +0600

    more date add

--- src/pages/Home/Notice/Notice.jsx
+++ src/pages/Home/Notice/Notice.jsx
@@ -6,7 +6,55 @@ const Notice = () => {
     <div>
       <div className="w-1/2">
         <div></div>
-        
+        {/* 1 */}
+        <div className="">
+        <div class="flex items-center">
+          <button class="bg-gray-200 p-2 rounded w-24 h-20">
+            <span class="text-3xl">18</span> <br />
+            <span class="text-xs">Feb 2024</span>
+          </button>
+          <p class="pl-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+            sed.hjjbbjbjbjbj
+          </p>
+          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
+        </div>
+        {/* <p className="pl-[480px]">read more</p> */}
+        <p className="border-b-4 pt-2 w-[580px]"></p>
+        </div>
+
+        <div className="">
+        <div class="flex items-center">
+          <button class="bg-gray-200 p-2 rounded w-24 h-20">
+            <span class="text-3xl">18</span> <br />
+            <span class="text-xs">Feb 2024</span>
+          </button>
+          <p class="pl-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+            sed.hjjbbjbjbjbj
+          </p>
+          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
+        </div>
+        {/* <p className="pl-[480px]">read more</p> */}
+        <p className="border-b-4 pt-2 w-[580px]"></p>
+        </div>
+
+        <div className="">
+        <div class="flex items-center">
+          <button class="bg-gray-200 p-2 rounded w-24 h-20">
+            <span class="text-3xl">18</span> <br />
+            <span class="text-xs">Feb 2024</span>
+          </button>
+          <p class="pl-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+            sed.hjjbbjbjbjbj
+          </p>
+          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
+        </div>
+        {/* <p className="pl-[480px]">read more</p> */}
+        <p className="border-b-4 pt-2 w-[580px]"></p>
+        </div>
+
         <div className="">
         <div class="flex items-center">
           <button class="bg-gray-200 p-2 rounded w-24 h-20">
