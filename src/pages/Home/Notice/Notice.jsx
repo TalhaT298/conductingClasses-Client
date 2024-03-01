d1457c8a9945eb3dcb1420eaf78b7bae88e40190
Talha <mdtalha2008@gmail.com>
2024-03-02 03:18:19 +0600

    notice upsate

--- src/pages/Home/Notice/Notice.jsx
+++ src/pages/Home/Notice/Notice.jsx
@@ -3,7 +3,7 @@ import { PiArrowRightLight } from "react-icons/pi";
 import { Link } from "react-router-dom";
 const Notice = () => {
   return (
-    <div>
+    <div className="flex">
       <div className="w-1/2">
         <div></div>
         {/* 1 */}
@@ -72,7 +72,77 @@ const Notice = () => {
         </div>
 
       </div>
-      <div className="w-1/2"></div>
+
+      {/* 2nd part right side */}
+      <div className="w-1/2">
+
+      <div className="">
+        <div class="flex items-center">
+          <button class="bg-gray-200 p-2 rounded w-24 h-20">
+            <span class="text-3xl">18</span> <br />
+            <span class="text-xs">Feb 2024</span>
+          </button>
+          <p class="pl-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+            sed.hjjbbjbjbjbj
+          </p>
+          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
+        </div>
+        {/* <p className="pl-[480px]">read more</p> */}
+        <p className="border-b-4 pt-2 w-[580px]"></p>
+        </div>
+
+        <div className="">
+        <div class="flex items-center">
+          <button class="bg-gray-200 p-2 rounded w-24 h-20">
+            <span class="text-3xl">18</span> <br />
+            <span class="text-xs">Feb 2024</span>
+          </button>
+          <p class="pl-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+            sed.hjjbbjbjbjbj
+          </p>
+          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
+        </div>
+        {/* <p className="pl-[480px]">read more</p> */}
+        <p className="border-b-4 pt-2 w-[580px]"></p>
+        </div>
+
+        <div className="">
+        <div class="flex items-center">
+          <button class="bg-gray-200 p-2 rounded w-24 h-20">
+            <span class="text-3xl">18</span> <br />
+            <span class="text-xs">Feb 2024</span>
+          </button>
+          <p class="pl-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+            sed.hjjbbjbjbjbj
+          </p>
+          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
+        </div>
+        {/* <p className="pl-[480px]">read more</p> */}
+        <p className="border-b-4 pt-2 w-[580px]"></p>
+        </div>
+
+        <div className="">
+        <div class="flex items-center">
+          <button class="bg-gray-200 p-2 rounded w-24 h-20">
+            <span class="text-3xl">18</span> <br />
+            <span class="text-xs">Feb 2024</span>
+          </button>
+          <p class="pl-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+            sed.hjjbbjbjbjbj
+          </p>
+          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
+        </div>
+        {/* <p className="pl-[480px]">read more</p> */}
+        <p className="border-b-4 pt-2 w-[580px]"></p>
+        </div>
+
+
+      </div>
+      
     </div>
   );
 };
