4c07fb135f07b629e44236eef5d3fa56828a9fe9
Talha <mdtalha2008@gmail.com>
2024-03-02 02:04:42 +0600

    notice find

--- src/pages/Home/RecentEvents/RecentEvents.jsx
+++ src/pages/Home/RecentEvents/RecentEvents.jsx
@@ -10,7 +10,7 @@ const RecentEvents = () => {
         // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
       ></SectionTitle>
       <div className="flex justify-center item-center">
-      <div className="card w-96 bg-base-100 shadow-xl mx-4">
+        <div className="card w-96 bg-base-100 shadow-xl mx-4">
           <figure className="px-4 pt-10">
             <img
               src="https://i.ibb.co/pxTDWWp/khela.webp"
@@ -21,11 +21,18 @@ const RecentEvents = () => {
           </figure>
           <div className="card-body items-center text-center">
             <h2 className="card-title">REC Cricket Tournament</h2>
-            <p>Every year REC Sports club held differnt type of sports tournament.Every year REC Sports club held differnt type of sports tournament.</p>
+            <p>
+              Every year REC Sports club held differnt type of sports
+              tournament.Every year REC Sports club held differnt type of sports
+              tournament.
+            </p>
             <div className="card-actions">
-             <Link to="/cricket">
-             <button className="btn btn-outline btn-info h-4 w-40">Read More<PiArrowRightLight size="1.1rem" color="blue" /></button>
-             </Link>
+              <Link to="/cricket">
+                <button className="btn btn-outline btn-info h-4 w-40">
+                  Read More
+                  <PiArrowRightLight size="1.1rem" color="blue" />
+                </button>
+              </Link>
             </div>
           </div>
         </div>
@@ -41,11 +48,18 @@ const RecentEvents = () => {
           </figure>
           <div className="card-body items-center text-center">
             <h2 className="card-title">Programming-Hero Seminar</h2>
-            <p>The REC Learniverse Club organizes this seminar, recognizing its profound impact on our future endeavors and successes. Such events play an invaluable role in shaping our later lives..</p>
+            <p>
+              The REC Learniverse Club organizes this seminar, recognizing its
+              profound impact on our future endeavors and successes. Such events
+              play an invaluable role in shaping our later lives..............
+            </p>
             <div className="card-actions">
-             <Link to="/cricket">
-             <button className="btn btn-outline btn-info h-4 w-40">Read More<PiArrowRightLight size="1.1rem" color="blue" /></button>
-             </Link>
+              <Link to="/cricket">
+                <button className="btn btn-outline btn-info h-4 w-40">
+                  Read More
+                  <PiArrowRightLight size="1.1rem" color="blue" />
+                </button>
+              </Link>
             </div>
           </div>
         </div>
@@ -58,14 +72,24 @@ const RecentEvents = () => {
               className="rounded-xl"
               style={{ width: "350px", height: "250px" }} // Increased width of the image
             />
+            <div className="absolute bottom-[276px] left-5 text-white text-xs bg-black p-1 rounded-md">
+              March 2, 2024
+            </div>
           </figure>
           <div className="card-body items-center text-center">
             <h2 className="card-title">REC Cricket Tournament</h2>
-            <p>Every year, REC Sports Club hosts diverse sports tournaments. This year, the CSE department emerged as champions, showcasing outstanding teamwork and skill..</p>
+            <p>
+              Every year, REC Sports Club hosts diverse sports tournaments. This
+              year, the CSE department emerged as champions, showcasing
+              outstanding teamwork and skill..
+            </p>
             <div className="card-actions">
-             <Link to="/cricket">
-             <button className="btn btn-outline btn-info h-10 w-40">Read More<PiArrowRightLight size="1.1rem" color="blue" /></button>
-             </Link>
+              <Link to="/cricket">
+                <button className="btn btn-outline btn-info h-10 w-40">
+                  Read More
+                  <PiArrowRightLight size="1.1rem" color="blue" />
+                </button>
+              </Link>
             </div>
           </div>
         </div>
