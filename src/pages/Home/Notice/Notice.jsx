e827594d7aa840ecb18ef2d40f022cf8cbd820ae
Talha <mdtalha2008@gmail.com>
2024-03-02 03:12:12 +0600

    button notice add

--- src/pages/Home/Notice/Notice.jsx
+++ src/pages/Home/Notice/Notice.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { PiArrowRightLight } from "react-icons/pi";
+import { Link } from "react-router-dom";
 const Notice = () => {
   return (
     <div>
       <div className="w-1/2">
         <div></div>
-
+        
         <div className="">
-        <div class="flex ">
-          <button class="bg-gray-200 p-2 rounded w-20 h-20">
+        <div class="flex items-center">
+          <button class="bg-gray-200 p-2 rounded w-24 h-20">
             <span class="text-3xl">18</span> <br />
             <span class="text-xs">Feb 2024</span>
           </button>
@@ -16,8 +17,10 @@ const Notice = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
             sed.hjjbbjbjbjbj
           </p>
+          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
         </div>
-        <p className="pl-[480px]">read more</p>
+        {/* <p className="pl-[480px]">read more</p> */}
+        <p className="border-b-4 pt-2 w-[580px]"></p>
         </div>
 
       </div>
