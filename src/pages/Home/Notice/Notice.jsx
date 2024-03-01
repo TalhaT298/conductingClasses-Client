59b74c39a02afcec9324fa30685aa6d61f297db7
Talha <mdtalha2008@gmail.com>
2024-03-02 02:24:09 +0600

    notice update

--- src/pages/Home/Notice/Notice.jsx
+++ src/pages/Home/Notice/Notice.jsx
@@ -5,8 +5,10 @@ const Notice = () => {
         <div>
             <div className='w-1/2'> 
                 <div></div>
-                <div>
-
+                <div className='flex'>
+                    <button><span>18</span> <br />Feb 2024</button>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, sed.</p>
+                    <p>read more</p>
                 </div>
             </div>
             <div className='w-1/2'></div>
