4fb0e8b4ef0daabf3c2a5ab54444df40e26a43ea
Talha <mdtalha2008@gmail.com>
2024-03-01 23:30:41 +0600

    increase size

--- src/pages/Home/RecentEvents/RecentEvents.jsx
+++ src/pages/Home/RecentEvents/RecentEvents.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import React from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const RecentEvents = () => {
-    return (
-        <div>
-            <SectionTitle
-            heading={"Recent Events"}
-            // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
-            >
-            </SectionTitle>
-            <div className="flex justify-center item-center">
+  return (
+    <div>
+      <SectionTitle
+        heading={"Recent Events"}
+        // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
+      ></SectionTitle>
+      <div className="flex justify-center item-center">
         <div className="card  bg-base-100 shadow-xl mx-4">
           <figure className="px-10 pt-10">
-          <img
+            <img
               src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
               alt="Shoes"
               className="rounded-xl"
-              style={{ width: '300px', height: '200px' }} // Adjust width and height as needed
+              style={{ width: "300px", height: "200px" }} // Adjust width and height as needed
             />
           </figure>
           <div className="card-body items-center text-center">
@@ -30,11 +29,11 @@ const RecentEvents = () => {
 
         <div className="card w-96 bg-base-100 shadow-xl mx-4">
           <figure className="px-10 pt-10">
-          <img
+            <img
               src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
               alt="Shoes"
               className="rounded-xl"
-              style={{ width: '300px', height: '200px' }} // Adjust width and height as needed
+              style={{ width: "300px", height: "200px" }} // Adjust width and height as needed
             />
           </figure>
           <div className="card-body items-center text-center">
@@ -47,25 +46,25 @@ const RecentEvents = () => {
         </div>
 
         <div className="card w-96 bg-base-100 shadow-xl mx-4">
-          <figure className="px-10 pt-10">
-          <img
+          <figure className="px-4 pt-10">
+            <img
               src="https://i.ibb.co/kytx0s5/khela.jpg"
               alt="Shoes"
               className="rounded-xl"
-              style={{ width: '300px', height: '200px' }} // Adjust width and height as needed
+              style={{ width: "350px", height: "250px" }} // Increased width of the image
             />
           </figure>
           <div className="card-body items-center text-center">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions">
+            <h2 className="card-title">REC Cricket Tournament</h2>
+            <p>Every year REC Sports club held differnt type of sports tournament.Every year REC Sports club held differnt type of sports tournament.</p>
+            <div className="card-actions w-20 h-8">
               <button className="btn btn-primary">Buy Now</button>
             </div>
           </div>
         </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default RecentEvents;
