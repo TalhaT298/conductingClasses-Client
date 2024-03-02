b47f7cc3dcf0e7ac22af37358b23f6bf8ea1cec5
Talha <mdtalha2008@gmail.com>
2024-03-03 01:07:37 +0600

    with 60% add

--- src/pages/Home/Offer/Offer.jsx
+++ src/pages/Home/Offer/Offer.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const Offer = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        
+        <div className="w-3/5">
+        <div className="card-body">
+          <h2 className="card-title">New album is released!</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+          </div>
         </div>
-    );
+        </div>
+
+        <div className="w-2/5">
+        <figure>
+          <img
+            src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+            alt="Album"
+          />
+        </figure>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Offer;
