a8bf1d59bd5e04ffccca3d516931cbd1ae9b520f
Talha <mdtalha2008@gmail.com>
2024-03-03 01:39:59 +0600

    we are offer add

--- src/pages/Home/Offer/Offer.jsx
+++ src/pages/Home/Offer/Offer.jsx
@@ -8,54 +8,116 @@ const Offer = () => {
           <div className="card-body">
             {/* left side----------- */}
             <div>
-              <h3></h3>
-              <p></p>
+              <h3>ggggggggggggggg</h3>
+              <p>fgggggggggggggggg</p>
               {/* part two div---------------------------------------------- */}
-              <div>
-                <div className="w-12">
-                  <div className="flex">
-                    <p className=" pt-4">
+              <div className="flex">
+                <div className="w-1/2">
+                  <div className="flex py-4">
+                    <p className="pt-4">
                       <AiOutlineFileProtect size="4.3rem" color="black" />
                     </p>
-                    <h2 className="text-2xl">Accommodation <br /><p className="text-sm">We are providing a secure and convenient living facility for our students. </p></h2>
-                    
+                    <h2 className="text-2xl">
+                      Accommodation <br />
+                      <p className="text-sm">
+                        We are providing a secure and convenient living facility
+                        for our students.{" "}
+                      </p>
+                    </h2>
                   </div>
-                </div>
 
-                <div className="w-12">
-                  <div className="flex">
-                    <p className=" pt-4">
+                  <div className="flex py-4">
+                    <p className="pt-4">
                       <AiOutlineFileProtect size="4.3rem" color="black" />
                     </p>
-                    <h2 className="text-2xl">Accommodation <br /><p className="text-sm">We are providing a secure and convenient living facility for our students. </p></h2>
-                    
+                    <h2 className="text-2xl">
+                      Accommodation <br />
+                      <p className="text-sm">
+                        We are providing a secure and convenient living facility
+                        for our students.{" "}
+                      </p>
+                    </h2>
                   </div>
-                </div>
 
-                <div className="w-12">
-                  <div className="flex">
-                    <p className=" pt-4">
+                  <div className="flex py-4">
+                    <p className="pt-4">
                       <AiOutlineFileProtect size="4.3rem" color="black" />
                     </p>
-                    <h2 className="text-2xl">Accommodation <br /><p className="text-sm">We are providing a secure and convenient living facility for our students. </p></h2>
-                    
+                    <h2 className="text-2xl">
+                      Accommodation <br />
+                      <p className="text-sm">
+                        We are providing a secure and convenient living facility
+                        for our students.{" "}
+                      </p>
+                    </h2>
                   </div>
-                </div>
 
-                <div className="w-12">
-                  <div className="flex">
-                    <p className=" pt-4">
+                  <div className="flex py-4">
+                    <p className="pt-4">
                       <AiOutlineFileProtect size="4.3rem" color="black" />
                     </p>
-                    <h2 className="text-2xl">Accommodation <br /><p className="text-sm">We are providing a secure and convenient living facility for our students. </p></h2>
-                    
+                    <h2 className="text-2xl">
+                      Accommodation <br />
+                      <p className="text-sm">
+                        We are providing a secure and convenient living facility
+                        for our students.{" "}
+                      </p>
+                    </h2>
                   </div>
                 </div>
-                {/* another 50% */}
-                <div className="w-12">
+                {/* another ----------------------------------------------------------------------------*/}
+                <div className="w-1/2">
+                  <div className="flex py-4">
+                    <p className="pt-4">
+                      <AiOutlineFileProtect size="4.3rem" color="black" />
+                    </p>
+                    <h2 className="text-2xl">
+                      Accommodation <br />
+                      <p className="text-sm">
+                        We are providing a secure and convenient living facility
+                        for our students.{" "}
+                      </p>
+                    </h2>
+                  </div>
 
-                
+                  <div className="flex py-4">
+                    <p className="pt-4">
+                      <AiOutlineFileProtect size="4.3rem" color="black" />
+                    </p>
+                    <h2 className="text-2xl ">
+                      Accommodation <br />
+                      <p className="text-sm">
+                        We are providing a secure and convenient living facility
+                        for our students.{" "}
+                      </p>
+                    </h2>
+                  </div>
 
+                  <div className="flex py-4">
+                    <p className="pt-4">
+                      <AiOutlineFileProtect size="4.3rem" color="black" />
+                    </p>
+                    <h2 className="text-2xl">
+                      Accommodation <br />
+                      <p className="text-sm">
+                        We are providing a secure and convenient living facility
+                        for our students.{" "}
+                      </p>
+                    </h2>
+                  </div>
+
+                  <div className="flex py-4">
+                    <p className="pt-4">
+                      <AiOutlineFileProtect size="4.3rem" color="black" />
+                    </p>
+                    <h2 className="text-2xl">
+                      Accommodation <br />
+                      <p className="text-sm">
+                        We are providing a secure and convenient living facility
+                        for our students.{" "}
+                      </p>
+                    </h2>
+                  </div>
                 </div>
               </div>
               {/* part two div---------------------------------------------- */}
