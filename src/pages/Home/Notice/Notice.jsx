740c4354a9f094af4e5d48d0c67b817ac5b10c20
Talha <mdtalha2008@gmail.com>
2024-03-02 18:29:06 +0600

    view all add

--- src/pages/Home/Notice/Notice.jsx
+++ src/pages/Home/Notice/Notice.jsx
@@ -5,136 +5,188 @@ const Notice = () => {
   return (
     <div className="flex">
       <div className="w-1/2">
-        <div className="">
-        <h3 className='text-2xl uppercase border-y-2 py-2'>Notice</h3> 
+        <div className="flex">
+          <h3 className="text-2xl uppercase border-y-2 py-2">Notice</h3>
+          
         </div>
         {/* 1 */}
         <div className="py-2">
-        <div class="flex items-center">
-          <button class="bg-gray-200 p-2 rounded w-24 h-20">
-            <span class="text-3xl">18</span> <br />
-            <span class="text-xs">Feb 2024</span>
-          </button>
-          <p class="pl-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
-            sed.hjjbbjbjbjbj
-          </p>
-          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
-        </div>
-        {/* <p className="pl-[480px]">read more</p> */}
-        <p className="border-b-4 pt-2 w-[580px]"></p>
-        </div>
-
-        <div className="">
-        <div class="flex items-center">
-          <button class="bg-gray-200 p-2 rounded w-24 h-20">
-            <span class="text-3xl">18</span> <br />
-            <span class="text-xs">Feb 2024</span>
-          </button>
-          <p class="pl-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
-            sed.hjjbbjbjbjbj
-          </p>
-          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
-        </div>
-        {/* <p className="pl-[480px]">read more</p> */}
-        <p className="border-b-4 pt-2 w-[580px]"></p>
+          <div class="flex items-center">
+            <button class="bg-gray-200 p-2 rounded w-24 h-20">
+              <span class="text-3xl">18</span> <br />
+              <span class="text-xs">Feb 2024</span>
+            </button>
+            <p class="pl-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio, sed.hjjbbjbjbjbj
+            </p>
+            <Link to="">
+              <p className="pt-12 pr-16">
+                <PiArrowRightLight size="1.3rem" color="black" />
+              </p>
+            </Link>
+          </div>
+          {/* <p className="pl-[480px]">read more</p> */}
+          <p className="border-b-4 pt-2 w-[580px]"></p>
         </div>
 
         <div className="">
-        <div class="flex items-center">
-          <button class="bg-gray-200 p-2 rounded w-24 h-20">
-            <span class="text-3xl">18</span> <br />
-            <span class="text-xs">Feb 2024</span>
-          </button>
-          <p class="pl-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
-            sed.hjjbbjbjbjbj
-          </p>
-          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
-        </div>
-        {/* <p className="pl-[480px]">read more</p> */}
-        <p className="border-b-4 pt-2 w-[580px]"></p>
+          <div class="flex items-center">
+            <button class="bg-gray-200 p-2 rounded w-24 h-20">
+              <span class="text-3xl">18</span> <br />
+              <span class="text-xs">Feb 2024</span>
+            </button>
+            <p class="pl-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio, sed.hjjbbjbjbjbj
+            </p>
+            <Link to="">
+              <p className="pt-12 pr-16">
+                <PiArrowRightLight size="1.3rem" color="black" />
+              </p>
+            </Link>
+          </div>
+          {/* <p className="pl-[480px]">read more</p> */}
+          <p className="border-b-4 pt-2 w-[580px]"></p>
         </div>
 
         <div className="">
-        <div class="flex items-center">
-          <button class="bg-gray-200 p-2 rounded w-24 h-20">
-            <span class="text-3xl">18</span> <br />
-            <span class="text-xs">Feb 2024</span>
-          </button>
-          <p class="pl-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
-            sed.hjjbbjbjbjbj
-          </p>
-          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
-        </div>
-        {/* <p className="pl-[480px]">read more</p> */}
-        <p className="border-b-4 pt-2 w-[580px]"></p>
+          <div class="flex items-center">
+            <button class="bg-gray-200 p-2 rounded w-24 h-20">
+              <span class="text-3xl">18</span> <br />
+              <span class="text-xs">Feb 2024</span>
+            </button>
+            <p class="pl-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio, sed.hjjbbjbjbjbj
+            </p>
+            <Link to="">
+              <p className="pt-12 pr-16">
+                <PiArrowRightLight size="1.3rem" color="black" />
+              </p>
+            </Link>
+          </div>
+          {/* <p className="pl-[480px]">read more</p> */}
+          <p className="border-b-4 pt-2 w-[580px]"></p>
         </div>
 
+        <div className="">
+          <div class="flex items-center">
+            <button class="bg-gray-200 p-2 rounded w-24 h-20">
+              <span class="text-3xl">18</span> <br />
+              <span class="text-xs">Feb 2024</span>
+            </button>
+            <p class="pl-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio, sed.hjjbbjbjbjbj
+            </p>
+            <Link to="">
+              <p className="pt-12 pr-16">
+                <PiArrowRightLight size="1.3rem" color="black" />
+              </p>
+            </Link>
+          </div>
+          {/* <p className="pl-[480px]">read more</p> */}
+          <p className="border-b-4 pt-2 w-[580px]"></p>
+        </div>
       </div>
 
       {/* 2nd part right side */}
       <div className="w-1/2">
-      <div className="">
-        <h3 className='text-2xl uppercase border-y-2 py-2'>Important Dates</h3> 
+        <div className="flex ">
+          <h3 className="text-2xl uppercase border-y-2 py-2">
+            Important Dates
+          </h3>
+         <div className="pl-72 pt-4">
+         <button className="btn btn-sm btn-outline btn-info w-20">
+            View All
+          </button>
+         </div>
         </div>
-      <div className="py-2">
-        <div class="flex items-center">
-          <img src="https://i.ibb.co/C86wQFv/11.png" className="w-24 h-20" alt="" />
-          <p class="pl-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
-            sed.hjjbbjbjbjbj
-          </p>
-          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
-        </div>
-        {/* <p className="pl-[480px]">read more</p> */}
-        <p className="border-b-4 pt-2 w-[580px]"></p>
-        </div>
-
-        <div className="">
-        <div class="flex items-center">
-          <img src="https://i.ibb.co/C86wQFv/11.png" className="w-24 h-20" alt="" />
-          <p class="pl-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
-            sed.hjjbbjbjbjbj
-          </p>
-          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
-        </div>
-        {/* <p className="pl-[480px]">read more</p> */}
-        <p className="border-b-4 pt-2 w-[580px]"></p>
-        </div>
-
-        <div className="">
-        <div class="flex items-center">
-          <img src="https://i.ibb.co/C86wQFv/11.png" className="w-24 h-20" alt="" />
-          <p class="pl-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
-            sed.hjjbbjbjbjbj
-          </p>
-          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
-        </div>
-        {/* <p className="pl-[480px]">read more</p> */}
-        <p className="border-b-4 pt-2 w-[580px]"></p>
+        <div className="py-2">
+          <div class="flex items-center">
+            <img
+              src="https://i.ibb.co/C86wQFv/11.png"
+              className="w-24 h-20"
+              alt=""
+            />
+            <p class="pl-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio, sed.hjjbbjbjbjbj
+            </p>
+            <Link to="">
+              <p className="pt-12 pr-16">
+                <PiArrowRightLight size="1.3rem" color="black" />
+              </p>
+            </Link>
+          </div>
+          {/* <p className="pl-[480px]">read more</p> */}
+          <p className="border-b-4 pt-2 w-[580px]"></p>
         </div>
 
         <div className="">
-        <div class="flex items-center">
-          <img src="https://i.ibb.co/C86wQFv/11.png" className="w-24 h-20" alt="" />
-          <p class="pl-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
-            sed.hjjbbjbjbjbj
-          </p>
-          <Link to=""><p className="pt-12 pr-16"><PiArrowRightLight size="1.3rem" color="black" /></p></Link>
-        </div>
-        {/* <p className="pl-[480px]">read more</p> */}
-        <p className="border-b-4 pt-2 w-[580px]"></p>
+          <div class="flex items-center">
+            <img
+              src="https://i.ibb.co/C86wQFv/11.png"
+              className="w-24 h-20"
+              alt=""
+            />
+            <p class="pl-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio, sed.hjjbbjbjbjbj
+            </p>
+            <Link to="">
+              <p className="pt-12 pr-16">
+                <PiArrowRightLight size="1.3rem" color="black" />
+              </p>
+            </Link>
+          </div>
+          {/* <p className="pl-[480px]">read more</p> */}
+          <p className="border-b-4 pt-2 w-[580px]"></p>
         </div>
 
+        <div className="">
+          <div class="flex items-center">
+            <img
+              src="https://i.ibb.co/C86wQFv/11.png"
+              className="w-24 h-20"
+              alt=""
+            />
+            <p class="pl-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio, sed.hjjbbjbjbjbj
+            </p>
+            <Link to="">
+              <p className="pt-12 pr-16">
+                <PiArrowRightLight size="1.3rem" color="black" />
+              </p>
+            </Link>
+          </div>
+          {/* <p className="pl-[480px]">read more</p> */}
+          <p className="border-b-4 pt-2 w-[580px]"></p>
+        </div>
 
+        <div className="">
+          <div class="flex items-center">
+            <img
+              src="https://i.ibb.co/C86wQFv/11.png"
+              className="w-24 h-20"
+              alt=""
+            />
+            <p class="pl-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio, sed.hjjbbjbjbjbj
+            </p>
+            <Link to="">
+              <p className="pt-12 pr-16">
+                <PiArrowRightLight size="1.3rem" color="black" />
+              </p>
+            </Link>
+          </div>
+          {/* <p className="pl-[480px]">read more</p> */}
+          <p className="border-b-4 pt-2 w-[580px]"></p>
+        </div>
       </div>
-      
     </div>
   );
 };
