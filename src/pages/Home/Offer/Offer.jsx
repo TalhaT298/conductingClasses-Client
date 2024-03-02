55d8e35c197a87357e0c61fe271480787170dde6
Talha <mdtalha2008@gmail.com>
2024-03-03 01:25:01 +0600

    done right side

--- src/pages/Home/Offer/Offer.jsx
+++ src/pages/Home/Offer/Offer.jsx
@@ -1,27 +1,76 @@
 import React from "react";
-
+import { AiOutlineFileProtect } from "react-icons/ai";
 const Offer = () => {
   return (
     <div>
       <div className="card lg:card-side bg-base-100 shadow-xl">
-        
         <div className="w-3/5">
-        <div className="card-body">
-          <h2 className="card-title">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Listen</button>
+          <div className="card-body">
+            {/* left side----------- */}
+            <div>
+              <h3></h3>
+              <p></p>
+              {/* part two div---------------------------------------------- */}
+              <div>
+                <div className="w-12">
+                  <div className="flex">
+                    <p className=" pt-4">
+                      <AiOutlineFileProtect size="4.3rem" color="black" />
+                    </p>
+                    <h2 className="text-2xl">Accommodation <br /><p className="text-sm">We are providing a secure and convenient living facility for our students. </p></h2>
+                    
+                  </div>
+                </div>
+
+                <div className="w-12">
+                  <div className="flex">
+                    <p className=" pt-4">
+                      <AiOutlineFileProtect size="4.3rem" color="black" />
+                    </p>
+                    <h2 className="text-2xl">Accommodation <br /><p className="text-sm">We are providing a secure and convenient living facility for our students. </p></h2>
+                    
+                  </div>
+                </div>
+
+                <div className="w-12">
+                  <div className="flex">
+                    <p className=" pt-4">
+                      <AiOutlineFileProtect size="4.3rem" color="black" />
+                    </p>
+                    <h2 className="text-2xl">Accommodation <br /><p className="text-sm">We are providing a secure and convenient living facility for our students. </p></h2>
+                    
+                  </div>
+                </div>
+
+                <div className="w-12">
+                  <div className="flex">
+                    <p className=" pt-4">
+                      <AiOutlineFileProtect size="4.3rem" color="black" />
+                    </p>
+                    <h2 className="text-2xl">Accommodation <br /><p className="text-sm">We are providing a secure and convenient living facility for our students. </p></h2>
+                    
+                  </div>
+                </div>
+                {/* another 50% */}
+                <div className="w-12">
+
+                
+
+                </div>
+              </div>
+              {/* part two div---------------------------------------------- */}
+            </div>
+            {/* left side----------- */}
           </div>
-        </div>
         </div>
 
         <div className="w-2/5">
-        <figure>
-          <img
-            src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
-            alt="Album"
-          />
-        </figure>
+          <figure>
+            <img
+              src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+              alt="Album"
+            />
+          </figure>
         </div>
       </div>
     </div>
