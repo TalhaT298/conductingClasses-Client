35f4ad0f709e7642fa956bddee73b7905ef33fe7
Talha <mdtalha2008@gmail.com>
2024-03-02 18:30:19 +0600

    calender logo add

--- src/pages/Home/Notice/Notice.jsx
+++ src/pages/Home/Notice/Notice.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { PiArrowRightLight } from "react-icons/pi";
 import { Link } from "react-router-dom";
+import { SlCalender } from "react-icons/sl";
 const Notice = () => {
   return (
     <div className="flex">
       <div className="w-1/2">
         <div className="flex">
           <h3 className="text-2xl uppercase border-y-2 py-2">Notice</h3>
-          
+          <div className="">
+          <SlCalender size="1.3rem" color="black" />
+          </div>
         </div>
         {/* 1 */}
         <div className="py-2">
