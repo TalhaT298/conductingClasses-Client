232138517b2e99db2f30764504c7a3c556d8edd7
Talha <mdtalha2008@gmail.com>
2024-03-05 00:17:49 +0600

    admission upate

--- src/pages/Home/Scholarship/Scholarship.jsx
+++ src/pages/Home/Scholarship/Scholarship.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import sh1 from "../../../assets/sh1.webp";
+import sh1 from "../../../assets/sh5.webp";
 import sh2 from "../../../assets/sh2.webp";
+import sh3 from "../../../assets/sh3.webp";
 const Scholarship = () => {
     return (
         <div className='max-w-[1200px]'>
             <div className="flex justify-center items-center">
                 <img className='w-[320px]' src={sh1} alt="" />
                 <img className='w-[320px]' src={sh2} alt="" />
-                <img className='w-[320px]' src={sh1} alt="" />
+                <img className='w-[320px]' src={sh3} alt="" />
                 <img className='w-[320px]' src={sh2} alt="" />
                  
             </div>
