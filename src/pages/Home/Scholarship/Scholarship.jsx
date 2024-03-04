81580947fb0b4c4d3353496def97c3902dc5c32b
Talha <mdtalha2008@gmail.com>
2024-03-04 23:48:23 +0600

    scholarship update

--- src/pages/Home/Scholarship/Scholarship.jsx
+++ src/pages/Home/Scholarship/Scholarship.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Scholarship = () => {
     return (
         <div>
-            
+            <div className=" flex flex-wrap justify-between">
+                <img className='w-[320px]' src={tom2} alt="" />
+                 <img className='w-[320px]' src={tom3} alt="" />
+            </div>
         </div>
     );
 };
