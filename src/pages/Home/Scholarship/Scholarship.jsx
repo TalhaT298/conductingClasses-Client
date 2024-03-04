6035f39f83a45c5ed09919510ef3972ad4aaf005
Talha <mdtalha2008@gmail.com>
2024-03-05 00:08:43 +0600

    size update

--- src/pages/Home/Scholarship/Scholarship.jsx
+++ src/pages/Home/Scholarship/Scholarship.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import sh1 from "../../../assets/sh1.webp";
+import sh2 from "../../../assets/sh2.webp";
 const Scholarship = () => {
     return (
-        <div>
-            <div className=" flex flex-wrap justify-between">
-                <img className='w-[320px]' src={tom2} alt="" />
-                 <img className='w-[320px]' src={tom3} alt="" />
+        <div className='max-w-[1200px]'>
+            <div className="flex justify-center items-center">
+                <img className='w-[320px]' src={sh1} alt="" />
+                <img className='w-[320px]' src={sh2} alt="" />
+                <img className='w-[320px]' src={sh1} alt="" />
+                <img className='w-[320px]' src={sh2} alt="" />
+                 
             </div>
         </div>
     );
