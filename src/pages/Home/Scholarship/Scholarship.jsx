02812f05203b2c1cf360762ea7b013b9fcf68c8e
Talha <mdtalha2008@gmail.com>
2024-03-05 00:48:12 +0600

    framer motion add

--- src/pages/Home/Scholarship/Scholarship.jsx
+++ src/pages/Home/Scholarship/Scholarship.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import React from "react";
 import sh1 from "../../../assets/sh5.webp";
 import sh2 from "../../../assets/sh2.webp";
 // import sh7 from "../../../assets/sh7.webp";
 import sh8 from "../../../assets/sh8.webp";
 import sh3 from "../../../assets/sh3.webp";
 const Scholarship = () => {
-    return (
-        <div className='max-w-[1200px]'>
-            <div className="flex justify-center items-center">
-                <img className='w-[320px]' src={sh1} alt="" />
-                <img className='w-[320px]' src={sh2} alt="" />
-                <img className='w-[320px]' src={sh8} alt="" />
-                {/* <img className='w-[320px]' src={sh7} alt="" /> */}
-                <img className='w-[320px]' src={sh3} alt="" />
-                 
-            </div>
+  return (
+    <div class="max-w-[1200px] mx-auto">
+      <div class="flex justify-center">
+        <div class="flex justify-center items-center">
+          <img class="w-[320px]" src={sh1} alt="" />
+          <img class="w-[320px]" src={sh2} alt="" />
+          <img class="w-[320px]" src={sh8} alt="" />
+          {/* <!-- <img class="w-[320px]" src={sh7} alt="" /> --> */}
+          <img class="w-[320px]" src={sh3} alt="" />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Scholarship;
