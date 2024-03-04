5b5d5aad962afb43c0b0b21ff4fa93071e4299b4
Talha <mdtalha2008@gmail.com>
2024-03-05 00:31:35 +0600

    scholrship update

--- src/pages/Home/Scholarship/Scholarship.jsx
+++ src/pages/Home/Scholarship/Scholarship.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import sh1 from "../../../assets/sh5.webp";
 import sh2 from "../../../assets/sh2.webp";
+// import sh7 from "../../../assets/sh7.webp";
+import sh8 from "../../../assets/sh8.webp";
 import sh3 from "../../../assets/sh3.webp";
 const Scholarship = () => {
     return (
@@ -8,8 +10,9 @@ const Scholarship = () => {
             <div className="flex justify-center items-center">
                 <img className='w-[320px]' src={sh1} alt="" />
                 <img className='w-[320px]' src={sh2} alt="" />
+                <img className='w-[320px]' src={sh8} alt="" />
+                {/* <img className='w-[320px]' src={sh7} alt="" /> */}
                 <img className='w-[320px]' src={sh3} alt="" />
-                <img className='w-[320px]' src={sh2} alt="" />
                  
             </div>
         </div>
