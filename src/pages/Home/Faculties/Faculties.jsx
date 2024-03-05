320103132e38ca66a2e2f92687220208da6460d7
Talha <mdtalha2008@gmail.com>
2024-03-06 01:59:46 +0600

    cse add

--- src/pages/Home/Faculties/Faculties.jsx
+++ src/pages/Home/Faculties/Faculties.jsx
@@ -1,19 +1,101 @@
-import React from 'react';
-import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import React from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Faculties = () => {
-    return (
-        <div>
-            <SectionTitle
-            heading={"Faculties"}
-            // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
-            >
-            </SectionTitle>
-            <div>
-                <p>To help you build your career and accomplish your goal, we are offering 38 undergraduate and master's programs under 4 faculties.</p>
+  return (
+    <div>
+      <SectionTitle
+        heading={"Faculties"}
+        // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
+      ></SectionTitle>
+      <div>
+        <p>
+          To help you build your career and accomplish your goal, we are
+          offering 38 undergraduate and master's programs under 4 faculties.
+        </p>
+      </div>
+      <div>
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 lg:w-1/4 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/200x200"
+                ></img>
+                <div className="w-full">
+                  <h2 className="title-font font-medium text-lg text-gray-900">
+                  Computer Science & Engineering
+                  </h2>
+                  <p className="mb-4">
+                  The Computer Science & Engineering department at REC is at the forefront of research in areas such as artificial intelligence (AI),machine learning,cybersecurity, and software engineering.Our researchers are dedicated to the development of algorithms, systems, and applications that can transform industries and improve the quality of life.
+                  </p>
+                </div>
+              </div>
             </div>
+            <div className="p-4 lg:w-1/4 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/201x201"
+                ></img>
+                <div className="w-full">
+                  <h2 className="title-font font-medium text-lg text-gray-900">
+                    Holden Caulfield
+                  </h2>
+                  <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                  <p className="mb-4">
+                    DIY tote bag drinking vinegar cronut adaptogen squid fanny
+                    pack vaporware.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/4 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/202x202"
+                ></img>
+                <div className="w-full">
+                  <h2 className="title-font font-medium text-lg text-gray-900">
+                    Atticus Finch
+                  </h2>
+                  <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                  <p className="mb-4">
+                    DIY tote bag drinking vinegar cronut adaptogen squid fanny
+                    pack vaporware.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/4 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/203x203"
+                ></img>
+                <div className="w-full">
+                  <h2 className="title-font font-medium text-lg text-gray-900">
+                    Henry Letham
+                  </h2>
+                  <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                  <p className="mb-4">
+                    DIY tote bag drinking vinegar cronut adaptogen squid fanny
+                    pack vaporware.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Faculties;
