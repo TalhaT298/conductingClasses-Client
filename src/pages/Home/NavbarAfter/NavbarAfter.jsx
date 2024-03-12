fb1382022a0ed313125cd9663e9e6b0104154ef9
Talha <mdtalha2008@gmail.com>
2024-03-13 01:44:07 +0600

    right side

--- src/pages/Home/NavbarAfter/NavbarAfter.jsx
+++ src/pages/Home/NavbarAfter/NavbarAfter.jsx
@@ -5,7 +5,7 @@ const NavbarAfter = () => {
     <div className="pt-16">
       <div className="navbar bg-base-100">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
