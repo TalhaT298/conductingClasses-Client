67aef46630adcbb07ec3add26d4dd634a241ca28
Talha <mdtalha2008@gmail.com>
2024-03-17 01:04:22 +0600

    section add

--- src/pages/CSE/CSE.jsx
+++ src/pages/CSE/CSE.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SectionTitle from '../../components/SectionTitle/SectionTitle';
+import React from "react";
+import { Link } from "react-router-dom";
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
 
 const CSE = () => {
-    return (
-        <div>
-            <Link to="/cse">
-            <SectionTitle
-        className="font-serif"
-        heading={"Recent Events"}
-        // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
-      ></SectionTitle>
-            
-            </Link>
+  return (
+    <div>
+      <Link to="/cse">
+        <div className="py-20">
+          <div className="mockup-browser border bg-base-300">
+            <div className="mockup-browser-toolbar">
+              <div className="input">https://cse.com</div>
+            </div>
+            {/* <div className="flex justify-center px-4 py-16 bg-base-200">
+              Hello!
+            </div> */}
+          </div>
+          <SectionTitle
+            className="font-serif"
+            heading={"Computer Science & Engineering"}
+            // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
+          ></SectionTitle>
         </div>
-    );
+        <div></div>
+      </Link>
+    </div>
+  );
 };
 
 export default CSE;
