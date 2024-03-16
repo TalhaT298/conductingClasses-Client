4a0dd225322aec51daca7c8e1a67162563062a0d
Talha <mdtalha2008@gmail.com>
2024-03-16 23:31:54 +0600

    link add at ce

--- src/pages/CE/CE.jsx
+++ src/pages/CE/CE.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CE = () => {
     return (
         <div>
-            
+            <Link to="/ce">
+            <p>ce</p>
+            </Link>
         </div>
     );
 };
