86e7c158aaa4a85394025ee9c578c5971ac683f2
Talha <mdtalha2008@gmail.com>
2024-03-16 23:28:20 +0600

    link add at cse

--- src/pages/CSE/CSE.jsx
+++ src/pages/CSE/CSE.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CSE = () => {
     return (
         <div>
+            <Link to="">
             
+            
+            </Link>
         </div>
     );
 };
