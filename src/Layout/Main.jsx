a7e36ecce63b8f23ceac0b959fa6700cd41e75bc
Talha <mdtalha2008@gmail.com>
2024-03-16 23:18:22 +0600

    location add

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -4,6 +4,9 @@ import Footer from '../pages/Shared/Footer/Footer';
 import Navbar from '../pages/Shared/Navbar/Navbar';
 
 const Main = () => {
+    const location = useLocation();
+    // console.log(location);
+    const noHeaderFooter= location.pathname.includes('login') || location.pathname.includes('signup');
     return (
         <div>
             <Navbar></Navbar>
