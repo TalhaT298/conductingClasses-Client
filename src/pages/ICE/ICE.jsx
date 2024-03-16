7734fde775ad8440f3586141e76f18623de27d1a
Talha <mdtalha2008@gmail.com>
2024-03-16 23:29:51 +0600

    link add at ice

--- src/pages/ICE/ICE.jsx
+++ src/pages/ICE/ICE.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ICE = () => {
     return (
         <div>
+            <Link to="/ice">
             
+            </Link>
         </div>
     );
 };
