24489385bff4b9f9e8145766bb56f9692d163d77
Talha <mdtalha2008@gmail.com>
2024-03-17 00:04:34 +0600

    section title add at cse

--- src/pages/CSE/CSE.jsx
+++ src/pages/CSE/CSE.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
 
 const CSE = () => {
     return (
         <div>
             <Link to="/cse">
-            
+            <SectionTitle
+        className="font-serif"
+        heading={"Recent Events"}
+        // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
+      ></SectionTitle>
             
             </Link>
         </div>
