84a5fbef1793fe301fa55bbb5baf5f00695cec68
Talha <mdtalha2008@gmail.com>
2024-03-16 23:33:18 +0600

    eee add at link

--- src/pages/EEE/EEE.jsx
+++ src/pages/EEE/EEE.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const EEE = () => {
     return (
         <div>
+            <Link to="/eee">
+            <p>eee</p>
             
+            </Link>
         </div>
     );
 };
