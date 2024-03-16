bec59760e7367e599fc914ac420dc138aac1bc44
Talha <mdtalha2008@gmail.com>
2024-03-16 18:46:43 +0600

    update redetails

--- src/pages/Home/RecentEvents/RecentEvents.jsx
+++ src/pages/Home/RecentEvents/RecentEvents.jsx
@@ -5,12 +5,13 @@ import { PiArrowRightLight } from "react-icons/pi";
 const RecentEvents = () => {
   return (
     <div className="px-4">
-      <SectionTitle className="font-serif"
+      <SectionTitle
+        className="font-serif"
         heading={"Recent Events"}
         // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
       ></SectionTitle>
       <div className="flex flex-col md:flex-row md:flex-wrap justify-center items-center">
-      <div className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96">
+        <div className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96">
           <figure className="px-4 pt-10">
             <img
               src="https://i.ibb.co/pxTDWWp/khela.webp"
@@ -26,15 +27,19 @@ const RecentEvents = () => {
             </p>
             <div className="card-actions flex justify-center items-center">
               <Link to="/cricket">
-                <button className="btn btn-outline w-full bg-black text-white border-black border hover:bg-white hover:text-black">
-                  Read More <PiArrowRightLight size="1.1rem" className="text-white group-hover:text-black" />
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
+                  Read More
+                  <PiArrowRightLight
+                    size="1.1rem"
+                    className="text-white group-hover:text-black"
+                  />
                 </button>
               </Link>
             </div>
           </div>
-          </div>
+        </div>
 
-          <div className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96">
+        <div className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96">
           <figure className="px-4 pt-10">
             <img
               src="https://i.ibb.co/pxTDWWp/khela.webp"
@@ -50,15 +55,19 @@ const RecentEvents = () => {
             </p>
             <div className="card-actions card-actions flex justify-center items-center">
               <Link to="/cricket">
-                <button className="btn btn-outline w-full bg-black text-white border-black border hover:bg-white hover:text-black">
-                  Read More <PiArrowRightLight size="1.1rem" className="text-white group-hover:text-black" />
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
+                  Read More
+                  <PiArrowRightLight
+                    size="1.1rem"
+                    className="text-white group-hover:text-black"
+                  />
                 </button>
               </Link>
             </div>
           </div>
-          </div>
+        </div>
 
-          <div className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96">
+        <div className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96">
           <figure className="px-4 pt-10">
             <img
               src="https://i.ibb.co/pxTDWWp/khela.webp"
@@ -74,13 +83,17 @@ const RecentEvents = () => {
             </p>
             <div className="card-actions card-actions flex justify-center items-center">
               <Link to="/cricket">
-                <button className="btn btn-outline w-full bg-black text-white border-black border hover:bg-white hover:text-black">
-                  Read More <PiArrowRightLight size="1.1rem" className="text-white group-hover:text-black" />
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
+                  Read More
+                  <PiArrowRightLight
+                    size="1.1rem"
+                    className="text-white group-hover:text-black"
+                  />
                 </button>
               </Link>
             </div>
           </div>
-          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/REDetails/REDetails.jsx
+++ src/pages/REDetails/REDetails.jsx
@@ -1,14 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// import React from "react";
+// import { Link } from "react-router-dom";
 
-const REDetails = () => {
+// const REDetails = () => {
+//   return (
+//     <div>
+//       <Link to="/cricket">
+//       <div>
+//         <p>hggggggggggggggggggggg</p>
+//         <img src="https://i.ibb.co/pxTDWWp/khela.webp" alt="" />
+//       </div>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default REDetails;
+
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+export const REDetails = () => {
+    const [currentSlider, setCurrentSlider] = useState(0);
+    const sliders = ['https://i.ibb.co/Lz6PBDX/3.webp','https://i.ibb.co/CKQTWP9/2.webp','https://i.ibb.co/Lz6PBDX/3.webp','https://i.ibb.co/CKQTWP9/2.webp','https://i.ibb.co/Lz6PBDX/3.webp'];
+    const nextSlider = () => setCurrentSlider((currentSlider) => (currentSlider === sliders.length - 1 ? 0 : currentSlider + 1));
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            nextSlider();
+        }, 3000);
+        return () => clearInterval(intervalId);
+    }, [currentSlider]);
     return (
-        <div>
-            <Link to="/cricket"></Link>
-            <div>
-                <p>h</p>
-                <img src="https://i.ibb.co/pxTDWWp/khela.webp" alt="" />
+        <div className=''>
+            <Link to="/cricket">
+            <div className="w-full mx-auto h-[240px] md:h-[470px] lg:h-[600px] flex flex-col lg:flex-row items-center justify-center overflow-hidden gap-5 lg:gap-10 px-10">
+                <div className="relative overflow-hidden">
+                    {/* dots */}
+                    <div className="flex h-fit w-fit rounded-full z-50 absolute right-0 bottom-1/2 w-fit gap-1 rotate-90">
+                        {sliders.map((_, inx) => (
+                            <button key={inx} onClick={() => setCurrentSlider(inx)}
+                                className={`rounded-full duration-300 bg-white ${currentSlider === inx ? 'w-10' : 'w-2'} h-2`}>
+                            </button>
+                        ))}
+                    </div>
+                    {/* slider container */}
+                    <div className="ease-linear duration-300 flex flex-col h-[340px] md:h-[670px] transform-gpu relative" style={{ transform: `translateY(-${currentSlider * 100}%)` }}>
+                        {/* sliders */}
+                        {sliders.map((_, inx) => (
+                            <div key={inx} className="min-w-full duration-200 before:content-['Image'] before:bg-black/20 before:-z-10 before:absolute before:text-3xl before:flex before:justify-center before:items-center before:text-black/40 before:inset-0 relative">
+                                <img src={_} className="w-full h-[340px] md:h-[670px] object-cover" alt={`Slider - ${inx + 1}`} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
+            </Link>
+        
         </div>
     );
 };
