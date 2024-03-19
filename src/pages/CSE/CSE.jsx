a0bba9cddc02d9c36d9ae1c96e60683c47777836
Talha <mdtalha2008@gmail.com>
2024-03-20 01:07:28 +0600

    head add

--- src/pages/CSE/CSE.jsx
+++ src/pages/CSE/CSE.jsx
@@ -71,14 +71,10 @@ const CSE = () => {
                   </div>
                   <div class="flex flex-col items-center text-center justify-center">
                     <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">
-                      Phoebe Caulfield
+                      Md.Tarikul Islam <br /> <span className="text-xs">Lecturer,B.Sc. in CSE(PUST)</span>
                     </h2>
                     <div class="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-                    <p class="text-base">
-                      Raclette knausgaard hella meggs normcore williamsburg
-                      enamel pin sartorial venmo tbh hot chicken gentrify
-                      portland.
-                    </p>
+                    <p class="text-base">Head of Department</p>
                   </div>
                 </div>
                 <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
