09a3f6216548b1a10887ddc1c2f12d3958975923
Talha <mdtalha2008@gmail.com>
2024-03-20 19:03:12 +0600

    at a glance add

--- src/pages/CSE/CSE.jsx
+++ src/pages/CSE/CSE.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import SectionTitle from "../../components/SectionTitle/SectionTitle";
-import LightGallery from "lightgallery/react";
-import lgZoom from "lightgallery/plugins/zoom";
-import lgVideo from "lightgallery/plugins/video";
+// import SectionTitle from "../../components/SectionTitle/SectionTitle";
+// import LightGallery from "lightgallery/react";
+// import lgZoom from "lightgallery/plugins/zoom";
+// import lgVideo from "lightgallery/plugins/video";
 import fjGallery from "flickr-justified-gallery";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -13,16 +13,43 @@ import { BiLogoGmail } from "react-icons/bi";
 import { motion } from "framer-motion";
 
 const CSE = () => {
+  // useEffect(() => {
+  //   fjGallery(document.querySelectorAll(".gallery"), {
+  //     itemSelector: ".gallery__item",
+  //     rowHeight: 180,
+  //     lastRow: "start",
+  //     gutter: 2,
+  //     rowHeightTolerance: 0.1,
+  //     calculateItemsHeight: false,
+  //   });
+  // }, []);
+  const [currentSlider, setCurrentSlider] = useState(0);
+  const carouselImages = [
+    "https://source.unsplash.com/1200x540/?nature",
+    "https://source.unsplash.com/1200x540/?hill",
+    "https://source.unsplash.com/1200x540/?mountain",
+    "https://source.unsplash.com/1200x540/?river",
+    "https://source.unsplash.com/1200x540/?sea",
+  ];
+  const prevSlider = () =>
+    setCurrentSlider((currentSlider) =>
+      currentSlider === 0 ? carouselImages.length - 1 : currentSlider - 1
+    );
+  const nextSlider = useCallback(
+    () =>
+      setCurrentSlider((currentSlider) =>
+        currentSlider === carouselImages.length - 1 ? 0 : currentSlider + 1
+      ),
+    [carouselImages.length]
+  );
+
+  // if you don't want to change the slider automatically then you can just remove the useEffect
   useEffect(() => {
-    fjGallery(document.querySelectorAll(".gallery"), {
-      itemSelector: ".gallery__item",
-      rowHeight: 180,
-      lastRow: "start",
-      gutter: 2,
-      rowHeightTolerance: 0.1,
-      calculateItemsHeight: false,
-    });
-  }, []);
+    const intervalId = setInterval(() => {
+      nextSlider();
+    }, 3000);
+    return () => clearInterval(intervalId);
+  }, [nextSlider]);
   return (
     <div>
       <Link to="/cse">
@@ -42,7 +69,7 @@ const CSE = () => {
           ></SectionTitle> */}
           <div className="mx-auto md:w-8/12 my-8 flex justify-center items-center ">
             {/* <p className='text-yellow-600'>----{subHeading}---</p> */}
-            <h3 className="text-4xl uppercase border-y-2 py-4 font-bold font-serif">
+            <h3 className="text-4xl uppercase border-y-2 pt-4 font-bold font-serif">
               Computer Science & Engineering
             </h3>
           </div>
@@ -82,48 +109,48 @@ const CSE = () => {
                         Lecturer,B.Sc. in CSE(PUST)
                       </span>
                     </h2>
-                    <div class="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+                    <div class="w-12 h-1 bg-black rounded mt-2 mb-4"></div>
                     <p class="text-base">Head of Department</p>
                     <div className="grid grid-flow-col gap-3">
-                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                      <Link to="">
-                        <a href="">
-                          <FaFacebookF size="1.7rem" color="black" />
-                        </a>
-                      </Link>
-                    </motion.div>
+                      <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                        <Link to="">
+                          <a href="">
+                            <FaFacebookF size="1.7rem" color="black" />
+                          </a>
+                        </Link>
+                      </motion.div>
 
-                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                      <Link to="">
-                        <a href="">
-                          <FaInstagram size="1.7rem" color="black" />
-                        </a>
-                      </Link>
-                    </motion.div>
+                      <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                        <Link to="">
+                          <a href="">
+                            <FaInstagram size="1.7rem" color="black" />
+                          </a>
+                        </Link>
+                      </motion.div>
 
-                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                      <Link to="">
-                        <a href="">
-                          <FaLinkedinIn size="1.7rem" color="black" />
-                        </a>
-                      </Link>
-                    </motion.div>
+                      <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                        <Link to="">
+                          <a href="">
+                            <FaLinkedinIn size="1.7rem" color="black" />
+                          </a>
+                        </Link>
+                      </motion.div>
 
-                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                      <Link to="">
-                        <a href="">
-                          <FaWhatsapp size="1.7rem" color="black" />
-                        </a>
-                      </Link>
-                    </motion.div>
-                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                      <Link to="">
-                        <a href="">
-                          <BiLogoGmail size="1.7rem" color="black" />
-                        </a>
-                      </Link>
-                    </motion.div>
-                  </div>
+                      <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                        <Link to="">
+                          <a href="">
+                            <FaWhatsapp size="1.7rem" color="black" />
+                          </a>
+                        </Link>
+                      </motion.div>
+                      <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                        <Link to="">
+                          <a href="">
+                            <BiLogoGmail size="1.7rem" color="black" />
+                          </a>
+                        </Link>
+                      </motion.div>
+                    </div>
                   </div>
                 </div>
                 <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
@@ -144,7 +171,7 @@ const CSE = () => {
                     forward to meeting and working with each one of you. Best
                     wishes for a fruitful and rewarding academic journey
                   </p>
-                  <a class="text-indigo-500 inline-flex items-center">
+                  <a class="text-black inline-flex items-center">
                     Learn More
                     <svg
                       fill="none"
@@ -166,7 +193,7 @@ const CSE = () => {
 
         {/* -----------------------------------3rd section start---------------------------------------- */}
         <section class="text-gray-600 body-font">
-          <div class="container px-5 py-24 mx-auto">
+          <div class="container px-5 py-12 mx-auto">
             <div class="flex flex-wrap w-full mb-20">
               <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
                 <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
@@ -294,7 +321,7 @@ const CSE = () => {
                     alt="content"
                   ></img>
                   <h3 class="tracking-widest text-black text-xs font-medium title-font">
-                     Lecturer,B.Sc. in CSE(SUST)
+                    Lecturer,B.Sc. in CSE(SUST)
                   </h3>
                   <h2 class="text-lg text-gray-900 font-medium title-font mb-4">
                     Jnananjan Roy
@@ -396,12 +423,99 @@ const CSE = () => {
                   </div>
                 </div>
               </div>
-              
             </div>
           </div>
         </section>
         {/* -----------------------------------3rd section finish--------------------------------------- */}
-        <div>
+        {/* ---------------------------------------4th section start------------------------------------- */}
+        <div className="mx-auto md:w-8/12 my-8 flex justify-center items-center ">
+            {/* <p className='text-yellow-600'>----{subHeading}---</p> */}
+            <h3 className="text-4xl uppercase border-y-2 pt-4 font-bold font-serif">
+              At a glance
+            </h3>
+          </div>
+        <div className="h-60 w-full md:h-[470px] lg:h-[540px] relative overflow-hidden">
+          {/* arrow left */}
+          <button
+            onClick={prevSlider}
+            className="absolute top-1/2 left-3 z-50 flex justify-center items-center bg-white rounded-full w-6 h-6 md:w-8 md:h-8"
+          >
+            <svg
+              className="w-4 h-4 md:w-6 md:h-6 icon"
+              viewBox="0 0 1024 1024"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#000000"
+            >
+              <g strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <path
+                  fill="#0095FF"
+                  d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
+                ></path>
+              </g>
+            </svg>
+          </button>
+          {/* arrow right */}
+          <button
+            onClick={nextSlider}
+            className="absolute top-1/2 z-50 right-3  flex justify-center items-center bg-white rounded-full w-6 h-6 md:w-8 md:h-8"
+          >
+            <svg
+              className="w-4 h-4 md:w-6 md:h-6 icon"
+              viewBox="0 0 1024 1024"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#000000"
+              transform="rotate(180)"
+            >
+              <g strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <path
+                  fill="#0095FF"
+                  d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
+                ></path>
+              </g>
+            </svg>
+          </button>
+          {/* dots */}
+          <div className="flex justify-center items-center rounded-full z-50 absolute bottom-4 w-full gap-1">
+            {carouselImages.map((_, inx) => (
+              <button
+                key={_}
+                onClick={() => setCurrentSlider(inx)}
+                className={`rounded-full duration-500 bg-white ${
+                  currentSlider === inx ? "w-8" : "wz-2"
+                } h-2`}
+              ></button>
+            ))}
+          </div>
+          {/* Carousel container */}
+          <div
+            className="ease-linear duration-500 flex transform-gpu"
+            style={{ transform: `translateX(-${currentSlider * 100}%)` }}
+          >
+            {/* sliders */}
+            {carouselImages.map((slide, inx) => (
+              <img
+                key={slide}
+                src={slide}
+                className="min-w-full h-60 bg-black/20 sm:h-96 md:h-[540px] object-cover"
+                alt={`Slider - ${inx + 1}`}
+              />
+            ))}
+          </div>
+        </div>
+        {/* ---------------------------------------4th section finish------------------------------------- */}
+        {/* <div>
           <LightGallery
             plugins={[lgZoom, lgVideo]}
             mode="lg-fade"
@@ -693,7 +807,10 @@ const CSE = () => {
               />
             </a>
           </LightGallery>
-        </div>
+        </div> */}
+        {/* ------------------------------------------5th section start--------------------------------- */}
+        
+        {/* ------------------------------------------5th section finish--------------------------------- */}
       </Link>
     </div>
   );
