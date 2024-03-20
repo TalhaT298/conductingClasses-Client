5755fb23a4bf9fa9714eae3d6e3f1394c824a832
Talha <mdtalha2008@gmail.com>
2024-03-20 19:51:18 +0600

    link add students

--- src/pages/CSE/CSE.jsx
+++ src/pages/CSE/CSE.jsx
@@ -808,10 +808,9 @@ const CSE = () => {
           </LightGallery>
         </div> */}
         {/* ------------------------------------------5th section start--------------------------------- */}
-        Explain
         <section class="text-gray-600 body-font">
           <div class="container px-5 py-24 mx-auto">
-            <div class="flex flex-col text-center w-full mb-20">
+            {/* <div class="flex flex-col text-center w-full mb-20">
               <h1 class="text-2xl font-medium title-font mb-4 text-gray-900 tracking-widest">
                 OUR TEAM
               </h1>
@@ -820,6 +819,12 @@ const CSE = () => {
                 gentrify, subway tile poke farm-to-table. Franzen you probably
                 haven't heard of them.
               </p>
+            </div> */}
+            <div className="mx-auto md:w-8/12 my-8 flex justify-center items-center ">
+              {/* <p className='text-yellow-600'>----{subHeading}---</p> */}
+              <h3 className="text-4xl uppercase border-y-2 pt-4 font-bold font-serif text-black">
+                Student's Projet
+              </h3>
             </div>
             <div class="flex flex-wrap -m-4">
               <div class="p-4 lg:w-1/2">
@@ -838,47 +843,46 @@ const CSE = () => {
                       DIY tote bag drinking vinegar cronut adaptogen squid fanny
                       pack vaporware.
                     </p>
-                    <span class="inline-flex">
-                      <a class="text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                        </svg>
-                      </a>
-                      <a class="ml-2 text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                        </svg>
-                      </a>
-                      <a class="ml-2 text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                        </svg>
-                      </a>
-                    </span>
+                    <div className="flex gap-3">
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaFacebookF size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaInstagram size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaLinkedinIn size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaWhatsapp size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <BiLogoGmail size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                  </div>
                   </div>
                 </div>
               </div>
@@ -898,47 +902,46 @@ const CSE = () => {
                       DIY tote bag drinking vinegar cronut adaptogen squid fanny
                       pack vaporware.
                     </p>
-                    <span class="inline-flex">
-                      <a class="text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                        </svg>
-                      </a>
-                      <a class="ml-2 text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                        </svg>
-                      </a>
-                      <a class="ml-2 text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                        </svg>
-                      </a>
-                    </span>
+                    <div className="flex gap-3">
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaFacebookF size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaInstagram size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaLinkedinIn size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaWhatsapp size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <BiLogoGmail size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                  </div>
                   </div>
                 </div>
               </div>
@@ -958,47 +961,46 @@ const CSE = () => {
                       DIY tote bag drinking vinegar cronut adaptogen squid fanny
                       pack vaporware.
                     </p>
-                    <span class="inline-flex">
-                      <a class="text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                        </svg>
-                      </a>
-                      <a class="ml-2 text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                        </svg>
-                      </a>
-                      <a class="ml-2 text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                        </svg>
-                      </a>
-                    </span>
+                    <div className="flex gap-3">
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaFacebookF size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaInstagram size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaLinkedinIn size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaWhatsapp size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <BiLogoGmail size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                  </div>
                   </div>
                 </div>
               </div>
@@ -1018,47 +1020,46 @@ const CSE = () => {
                       DIY tote bag drinking vinegar cronut adaptogen squid fanny
                       pack vaporware.
                     </p>
-                    <span class="inline-flex">
-                      <a class="text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                        </svg>
-                      </a>
-                      <a class="ml-2 text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                        </svg>
-                      </a>
-                      <a class="ml-2 text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                        </svg>
-                      </a>
-                    </span>
+                    <div className="flex gap-3">
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaFacebookF size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaInstagram size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaLinkedinIn size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaWhatsapp size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <BiLogoGmail size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                  </div>
                   </div>
                 </div>
               </div>
