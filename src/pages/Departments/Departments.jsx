b9fcfcfb573802b66438f37b6802a6b54b282be6
Talha <mdtalha2008@gmail.com>
2024-03-22 03:49:41 +0600

    Departments add

---
+++ src/pages/Departments/Departments.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Departments = () => {
+    return (
+        <div>
+            <Link to="/departments">
+            
+            </Link>
+        </div>
+    );
+};
+
+export default Departments;
