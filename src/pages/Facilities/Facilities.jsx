6229edb79f7a79b848d3d031ad47749982244c71
Talha <mdtalha2008@gmail.com>
2024-03-22 02:12:53 +0600

    facilities update

--- src/pages/Facilities/Facilities.jsx
+++ src/pages/Facilities/Facilities.jsx
@@ -17,6 +17,7 @@ const Facilities = () => {
               ></SectionTitle>
               <div class="flex flex-wrap -m-2">
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                  <Link to="">
                   <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
                     <img
                       alt="team"
@@ -29,126 +30,135 @@ const Facilities = () => {
                       </h2>
                     </div>
                   </div>
+                  </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                  <Link to="">
                   <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
                     <img
                       alt="team"
                       class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://dummyimage.com/84x84"
+                      src="https://dummyimage.com/80x80"
                     ></img>
-                    <div class="flex-grow">
-                      <h2 class="text-gray-900 title-font font-medium">
-                        Henry Letham
+                    <div class="flex-grow ml-8">
+                      <h2 class="text-black title-font text-3xl">
+                      Central Library
                       </h2>
-                      <p class="text-gray-500">CTO</p>
                     </div>
                   </div>
+                  </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                  <Link to="">
                   <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
                     <img
                       alt="team"
                       class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://dummyimage.com/88x88"
+                      src="https://dummyimage.com/80x80"
                     ></img>
-                    <div class="flex-grow">
-                      <h2 class="text-gray-900 title-font font-medium">
-                        Oskar Blinde
+                    <div class="flex-grow ml-8">
+                      <h2 class="text-black title-font text-3xl">
+                      Transport
                       </h2>
-                      <p class="text-gray-500">Founder</p>
                     </div>
                   </div>
+                  </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                  <Link to="">
                   <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
                     <img
                       alt="team"
                       class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://dummyimage.com/90x90"
+                      src="https://dummyimage.com/80x80"
                     ></img>
-                    <div class="flex-grow">
-                      <h2 class="text-gray-900 title-font font-medium">
-                        John Doe
+                    <div class="flex-grow ml-8">
+                      <h2 class="text-black title-font text-3xl">
+                      Alumni
                       </h2>
-                      <p class="text-gray-500">DevOps</p>
                     </div>
                   </div>
+                  </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                  <Link to="">
                   <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
                     <img
                       alt="team"
                       class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://dummyimage.com/94x94"
+                      src="https://dummyimage.com/80x80"
                     ></img>
-                    <div class="flex-grow">
-                      <h2 class="text-gray-900 title-font font-medium">
-                        Martin Eden
+                    <div class="flex-grow ml-8">
+                      <h2 class="text-black title-font text-3xl">
+                      Science Workshop
                       </h2>
-                      <p class="text-gray-500">Software Engineer</p>
                     </div>
                   </div>
+                  </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                  <Link to="">
                   <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
                     <img
                       alt="team"
                       class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://dummyimage.com/98x98"
+                      src="https://dummyimage.com/80x80"
                     ></img>
-                    <div class="flex-grow">
-                      <h2 class="text-gray-900 title-font font-medium">
-                        Boris Kitua
+                    <div class="flex-grow ml-8">
+                      <h2 class="text-black title-font text-3xl">
+                      Mental Health Center
                       </h2>
-                      <p class="text-gray-500">UX Researcher</p>
                     </div>
                   </div>
+                  </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                  <Link to="">
                   <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
                     <img
                       alt="team"
                       class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://dummyimage.com/100x90"
+                      src="https://dummyimage.com/80x80"
                     ></img>
-                    <div class="flex-grow">
-                      <h2 class="text-gray-900 title-font font-medium">
-                        Atticus Finch
+                    <div class="flex-grow ml-8">
+                      <h2 class="text-black title-font text-3xl">
+                      Physical Education
                       </h2>
-                      <p class="text-gray-500">QA Engineer</p>
                     </div>
                   </div>
+                  </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                  <Link to="">
                   <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
                     <img
                       alt="team"
                       class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://dummyimage.com/104x94"
+                      src="https://dummyimage.com/80x80"
                     ></img>
-                    <div class="flex-grow">
-                      <h2 class="text-gray-900 title-font font-medium">
-                        Alper Kamu
+                    <div class="flex-grow ml-8">
+                      <h2 class="text-black title-font text-3xl">
+                      Medical Center
                       </h2>
-                      <p class="text-gray-500">System</p>
                     </div>
                   </div>
+                  </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                  <Link to="">
                   <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
                     <img
                       alt="team"
                       class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://dummyimage.com/108x98"
+                      src="https://dummyimage.com/80x80"
                     ></img>
-                    <div class="flex-grow">
-                      <h2 class="text-gray-900 title-font font-medium">
-                        Rodrigo Monchi
+                    <div class="flex-grow ml-8">
+                      <h2 class="text-black title-font text-3xl">
+                      TSCC
                       </h2>
-                      <p class="text-gray-500">Product Manager</p>
                     </div>
                   </div>
+                  </Link>
                 </div>
               </div>
             </div>
