84307dc175af92109c02839eafa48edb31bc2c2f
Talha <mdtalha2008@gmail.com>
2024-03-22 01:29:34 +0600

    Facilities linked

--- src/pages/Facilities/Facilities.jsx
+++ src/pages/Facilities/Facilities.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Facilities = () => {
     return (
         <div>
+            <Link to="/facilities">
             
+            </Link>
         </div>
     );
 };
