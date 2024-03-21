df070d5f6d1061cf487df5e71eca17d281430163
Talha <mdtalha2008@gmail.com>
2024-03-22 03:19:25 +0600

    countup add

--- src/pages/Facilities/Facilities.jsx
+++ src/pages/Facilities/Facilities.jsx
@@ -13,151 +13,146 @@ const Facilities = () => {
               <SectionTitle
                 className="font-serif"
                 heading={"Facilities"}
-                
               ></SectionTitle>
               <div class="flex flex-wrap -m-2">
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                   <Link to="">
-                  <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="team"
-                      class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://i.ibb.co/1Zr0HxB/Screenshot-315-removebg-preview.png"
-                    ></img>
-                    <div class="flex-grow ml-8">
-                      <h2 class="text-black title-font text-3xl">
-                      ICT Center
-                      </h2>
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://i.ibb.co/1Zr0HxB/Screenshot-315-removebg-preview.png"
+                      ></img>
+                      <div class="flex-grow ml-8">
+                        <h2 class="text-black title-font text-3xl">
+                          ICT Center
+                        </h2>
+                      </div>
                     </div>
-                  </div>
                   </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                   <Link to="">
-                  <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="team"
-                      class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://i.ibb.co/zNXjpw7/Screenshot-322.png"
-                    ></img>
-                    <div class="flex-grow ml-8">
-                      <h2 class="text-black title-font text-3xl">
-                      Central Library
-                      </h2>
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://i.ibb.co/zNXjpw7/Screenshot-322.png"
+                      ></img>
+                      <div class="flex-grow ml-8">
+                        <h2 class="text-black title-font text-3xl">
+                          Central Library
+                        </h2>
+                      </div>
                     </div>
-                  </div>
                   </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                   <Link to="">
-                  <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="team"
-                      class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://i.ibb.co/Tg3DJKC/Screenshot-323-removebg-preview.png"
-                    ></img>
-                    <div class="flex-grow ml-8">
-                      <h2 class="text-black title-font text-3xl">
-                      Transport
-                      </h2>
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://i.ibb.co/Tg3DJKC/Screenshot-323-removebg-preview.png"
+                      ></img>
+                      <div class="flex-grow ml-8">
+                        <h2 class="text-black title-font text-3xl">
+                          Transport
+                        </h2>
+                      </div>
                     </div>
-                  </div>
                   </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                   <Link to="">
-                  <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="team"
-                      class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://i.ibb.co/WGHM35c/Screenshot-318-removebg-preview.png"
-                    ></img>
-                    <div class="flex-grow ml-8">
-                      <h2 class="text-black title-font text-3xl">
-                      Alumni
-                      </h2>
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://i.ibb.co/WGHM35c/Screenshot-318-removebg-preview.png"
+                      ></img>
+                      <div class="flex-grow ml-8">
+                        <h2 class="text-black title-font text-3xl">Alumni</h2>
+                      </div>
                     </div>
-                  </div>
                   </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                   <Link to="">
-                  <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="team"
-                      class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://i.ibb.co/XsfgZQd/Screenshot-319-removebg-preview.png"
-                    ></img>
-                    <div class="flex-grow ml-8">
-                      <h2 class="text-black title-font text-3xl">
-                      Science Workshop
-                      </h2>
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://i.ibb.co/XsfgZQd/Screenshot-319-removebg-preview.png"
+                      ></img>
+                      <div class="flex-grow ml-8">
+                        <h2 class="text-black title-font text-3xl">
+                          Science Workshop
+                        </h2>
+                      </div>
                     </div>
-                  </div>
                   </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                   <Link to="">
-                  <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="team"
-                      class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://i.ibb.co/qn68BLd/Screenshot-321-removebg-preview.png"
-                    ></img>
-                    <div class="flex-grow ml-8">
-                      <h2 class="text-black title-font text-3xl">
-                      Mental Health Center
-                      </h2>
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://i.ibb.co/qn68BLd/Screenshot-321-removebg-preview.png"
+                      ></img>
+                      <div class="flex-grow ml-8">
+                        <h2 class="text-black title-font text-3xl">
+                          Mental Health Center
+                        </h2>
+                      </div>
                     </div>
-                  </div>
                   </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                   <Link to="">
-                  <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="team"
-                      class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://i.ibb.co/54wQwyn/Screenshot-324-removebg-preview.png"
-                    ></img>
-                    <div class="flex-grow ml-8">
-                      <h2 class="text-black title-font text-3xl">
-                      Physical Education
-                      </h2>
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://i.ibb.co/54wQwyn/Screenshot-324-removebg-preview.png"
+                      ></img>
+                      <div class="flex-grow ml-8">
+                        <h2 class="text-black title-font text-3xl">
+                          Physical Education
+                        </h2>
+                      </div>
                     </div>
-                  </div>
                   </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                   <Link to="">
-                  <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="team"
-                      class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://i.ibb.co/kg9p09b/Screenshot-325-removebg-preview.png"
-                    ></img>
-                    <div class="flex-grow ml-8">
-                      <h2 class="text-black title-font text-3xl">
-                      Medical Center
-                      </h2>
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://i.ibb.co/kg9p09b/Screenshot-325-removebg-preview.png"
+                      ></img>
+                      <div class="flex-grow ml-8">
+                        <h2 class="text-black title-font text-3xl">
+                          Medical Center
+                        </h2>
+                      </div>
                     </div>
-                  </div>
                   </Link>
                 </div>
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                   <Link to="">
-                  <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="team"
-                      class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://i.ibb.co/47s67Y5/Screenshot-326-removebg-preview.png"
-                    ></img>
-                    <div class="flex-grow ml-8">
-                      <h2 class="text-black title-font text-3xl">
-                      TSCC
-                      </h2>
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        class="w-20 h-20 bg-white object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://i.ibb.co/47s67Y5/Screenshot-326-removebg-preview.png"
+                      ></img>
+                      <div class="flex-grow ml-8">
+                        <h2 class="text-black title-font text-3xl">TSCC</h2>
+                      </div>
                     </div>
-                  </div>
                   </Link>
                 </div>
               </div>
@@ -165,6 +160,72 @@ const Facilities = () => {
           </section>
         </div>
         {/* -------------------------------------1st end------------------------------------ */}
+        {/* ------------------------------2nd-start----------------------------------- */}
+        <div className="stats shadow w-full ">
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-8 h-8 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+            </div>
+            <div className="stat-title">Downloads</div>
+            <div className="stat-value">31K</div>
+            <div className="stat-desc">Jan 1st - Feb 1st</div>
+          </div>
+
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-8 h-8 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                ></path>
+              </svg>
+            </div>
+            <div className="stat-title">New Users</div>
+            <div className="stat-value">4,200</div>
+            <div className="stat-desc">↗︎ 400 (22%)</div>
+          </div>
+
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-8 h-8 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+                ></path>
+              </svg>
+            </div>
+            <div className="stat-title">New Registers</div>
+            <div className="stat-value">1,200</div>
+            <div className="stat-desc">↘︎ 90 (14%)</div>
+          </div>
+        </div>
+        {/* ------------------------------2nd-end----------------------------------- */}
       </Link>
     </div>
   );
