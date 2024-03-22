ad1283ff56f8ff37c7bbe78ce2ddb547cbd69519
Talha <mdtalha2008@gmail.com>
2024-03-23 00:42:04 +0600

    link add

--- src/pages/Teacher/Teacher.jsx
+++ src/pages/Teacher/Teacher.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Teacher = () => {
     return (
         <div>
-            
+           <Link to="/teacher">
+           
+           </Link> 
         </div>
     );
 };
