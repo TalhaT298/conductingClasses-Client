754cc424704bf45a0adbc002f055609f6e805467
Talha <mdtalha2008@gmail.com>
2024-03-23 15:05:11 +0600

    teacher update

--- src/pages/Teacher/Teacher.jsx
+++ src/pages/Teacher/Teacher.jsx
@@ -6,21 +6,21 @@ import { FaLinkedinIn } from "react-icons/fa6";
 import { FaWhatsapp } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
 import { motion } from "framer-motion";
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
 const Teacher = () => {
     return (
         <div>
            <Link to="/teacher">
+           
            {/* --------------------------------1st section start------------------- */}
-           <section class="text-gray-600 body-font">
+           <section class="text-black body-font">
+            
           <div class="container px-5 py-12 mx-auto">
-            <div class="flex flex-wrap w-full mb-20">
-              <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-                  Department Tutors
-                </h1>
-                <div class="h-1 w-20 bg-black rounded"></div>
-              </div>
-            </div>
+          <SectionTitle
+        className="font-serif"
+        heading={"Teacher"}
+        // subHeading={"National and International scholars, Business leaders, Entrepreneurs, Dignitaries visited us to share experiences, explore opportunities, collaborate and give guidance to the students"}
+      ></SectionTitle>
             <div class="flex flex-wrap -m-4">
               <div class="xl:w-1/4 md:w-1/2 p-4">
                 <div class="bg-gray-100 p-6 rounded-lg">
