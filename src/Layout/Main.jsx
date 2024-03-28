0b16c232651c9a58d8fb079718a3cfa298779c3b
Talha <mdtalha2008@gmail.com>
2024-03-29 02:52:04 +0600

    chatboat add

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
 import Navbar from '../pages/Shared/Navbar/Navbar';
+import ChatBot from '../pages/Shared/ChatBot/ChatBot';
 
 const Main = () => {
     const location = useLocation();
@@ -10,6 +11,7 @@ const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <ChatBot></ChatBot>
             <Outlet></Outlet>
             { noHeaderFooter || <Footer></Footer>}
         </div>
