67a2d03fce90cb956739f282f661a8c1a39cf2dd
Talha <mdtalha2008@gmail.com>
2024-04-07 02:30:47 +0600

    card add at admission

--- src/pages/Admission/Admission.jsx
+++ src/pages/Admission/Admission.jsx
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom";
 
 const Admission = () => {
   return (
-    <div className="pt-40">
-      
+    <div className="pt-40 flex">
       <div className="w-4/6">
-      <div className="font-serif text-center">
-        <p className="text-2xl">Admission & Routine 2023-24</p>
-      </div>
+        <div className="font-serif text-center">
+          <p className="text-2xl">Admission & Routine 2023-24</p>
+        </div>
         <div className="overflow-x-auto">
           <table className="table">
             {/* head */}
@@ -24,14 +23,22 @@ const Admission = () => {
               {/* row 1 */}
               <tr>
                 <th>1</th>
-                <td><Link to="">Practical Exam Time Schedule for Theatre Department</Link></td>
+                <td>
+                  <Link to="">
+                    Practical Exam Time Schedule for Theatre Department
+                  </Link>
+                </td>
                 <td> </td>
                 <td>2024-03-27</td>
               </tr>
               {/* row 2 */}
               <tr>
                 <th>2</th>
-                <td><Link to="">Practical Exam Time Schedule for Music Department</Link></td>
+                <td>
+                  <Link to="">
+                    Practical Exam Time Schedule for Music Department
+                  </Link>
+                </td>
                 <td></td>
                 <td>2024-03-27</td>
               </tr>
@@ -39,8 +46,10 @@ const Admission = () => {
               <tr>
                 <th>3</th>
                 <td>
-                <Link to="">Practical and Written Exam Notice For Fine Arts, Music,
-                  Theatre And English</Link>
+                  <Link to="">
+                    Practical and Written Exam Notice For Fine Arts, Music,
+                    Theatre And English
+                  </Link>
                 </td>
                 <td></td>
                 <td>2024-03-27</td>
@@ -48,14 +57,20 @@ const Admission = () => {
               {/* row 4 */}
               <tr>
                 <th>4</th>
-                <td><Link to="">Selected Candidates for English Written Examination</Link></td>
+                <td>
+                  <Link to="">
+                    Selected Candidates for English Written Examination
+                  </Link>
+                </td>
                 <td></td>
                 <td>2024-03-13</td>
               </tr>
               {/* row 5 */}
               <tr>
                 <th>5</th>
-                <td><Link to="">Notice For Admission Result</Link></td>
+                <td>
+                  <Link to="">Notice For Admission Result</Link>
+                </td>
                 <td></td>
                 <td>2024-03-13</td>
               </tr>
@@ -63,7 +78,17 @@ const Admission = () => {
           </table>
         </div>
       </div>
-      <div className="w-2/6"></div>
+      <div className="w-2/6">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title text-center">Undergraduate Application 2023-24</h2>
+            <p>Online Application & Result</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Final Application Guideline</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
