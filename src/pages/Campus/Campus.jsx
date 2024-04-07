e6762b30bdebaabf2308d6764e84a00016c7f5d6
Talha <mdtalha2008@gmail.com>
2024-04-07 09:32:01 +0600

    campus linked

--- src/pages/Campus/Campus.jsx
+++ src/pages/Campus/Campus.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Campus = () => {
     return (
+        
         <div>
-            
+            <Link to="/campus"></Link>
         </div>
     );
 };
