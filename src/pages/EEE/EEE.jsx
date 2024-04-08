5c1251cf2a2a75f30a93609bb637439c14e78421
Talha <mdtalha2008@gmail.com>
2024-04-09 03:08:39 +0600

    department add

--- src/pages/EEE/EEE.jsx
+++ src/pages/EEE/EEE.jsx
@@ -13,6 +13,7 @@ import { BiLogoGmail } from "react-icons/bi";
 import { motion } from "framer-motion";
 import polas from "../../assets/polas.jpg";
 import naz from "../../assets/MD. NAZMUL HASSAN.jpg";
+import nas from "../../assets/MdNasimHaidar.jpeg";
 
 const EEE = () => {
   // useEffect(() => {
@@ -90,34 +91,27 @@ const EEE = () => {
               </div> */}
             <div class="flex flex-col sm:flex-row mt-10">
               <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-                <div class="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-10 h-10"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
-                </div>
+              <div class="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                    <img
+                      src={nas}
+                      className="h-40 w-40 rounded-full"
+                      alt=""
+                    />
+                  </div>
                 <div class="flex flex-col items-center text-center justify-center">
                   <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">
-                    Md.Tarikul Islam <br />{" "}
-                    <span className="text-xs">Lecturer,B.Sc. in CSE(PUST)</span>
+                  Md. Nasim Haidar <br />{" "}
+                    <span className="text-xs">Lecturer,B.Sc. in EEE(RU)</span>
                   </h2>
                   <div class="w-12 h-1 bg-black rounded mt-2 mb-4"></div>
                   <p class="text-base">Head of Department</p>
                   <div className="grid grid-flow-col gap-3">
                     <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                      <Link to="">
-                        <a href="">
-                          <FaFacebookF size="1.7rem" color="black" />
+                       
+                        <a href="https://www.facebook.com/nasim.haidar.16">
+                          <FaFacebookF href="https://www.facebook.com/nasim.haidar.16" size="1.7rem" color="black" />
                         </a>
-                      </Link>
+                       
                     </motion.div>
 
                     <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
