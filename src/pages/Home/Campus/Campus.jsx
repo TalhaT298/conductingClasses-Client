ca4d5288f6d63b62caad050991987b15e3ac497d
Talha <mdtalha2008@gmail.com>
2024-04-11 18:26:03 +0600

    rec add

--- src/pages/Home/Campus/Campus.jsx
+++ src/pages/Home/Campus/Campus.jsx
@@ -6,17 +6,14 @@ import { Link } from "react-router-dom";
 const Campus = () => {
   return (
     <section className="featured-item bg-fixed text-black mb-12 pt-6 my-20">
-      <SectionTitle
-        subHeading={"check it out"}
-        heading={"Feature"}
-      ></SectionTitle>
+      <p className="uppercase text-5xl py-6 text-white font-extrabold text-center">rangpur engineering college</p>
       <div className="md:flex justify-center items-center py-20 pt-12 px-36">
-        <div className="w-1/2">
+        <div className="w-2/5">
           <img src={feature} alt="" />
         </div>
-        <div className="md:ml-10 w-1/2">
+        <div className="md:ml-10 w-3/5">
            
-          <p className="uppercase text-2xl text-white font-extrabold">rangpur engineering college</p>
+          {/* <p className="uppercase text-3xl text-white font-extrabold">rangpur engineering college</p> */}
           <p className="text-white font-extrabold">
           Rangpur Engineering College (REC) was established in the year 2014. It is affiliated by the University of Rajshahi. It is the First Non-Govt. Engineering College in Rangpur Division. This is the newly based Institute, imparting technical education leading to B.ScEngg. in the various disciplines of Engineering.
           </p>
