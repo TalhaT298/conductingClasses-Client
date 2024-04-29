9eaada68ff86875a090a1cf334495bd093f558b1
Talha <mdtalha2008@gmail.com>
2024-04-29 17:59:11 +0600

    comment remove add testimonial

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { Swiper, SwiperSlide } from "swiper/react";
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
 import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
-import "./Testimonial.css"; // Import your custom CSS file
+import "./Testimonial.css"; 
 import background from '../../../assets/lo.gif';
 
 
@@ -28,9 +27,9 @@ const Testimonial = () => {
           <SwiperSlide color="black" key={review._id}>
             <div className="flex flex-col items-center mx-24 my-8">
               <div className="pb-4">
-                {/* <TfiThought className="" size='4.7rem' color='black'></TfiThought> */}
+                 
               </div>
-              {/* <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly /> */}
+              
               <img
                 className="w-40 rounded-full"
                 src={review.img}
