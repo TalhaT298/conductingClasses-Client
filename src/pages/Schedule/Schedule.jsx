1fc1bf7aa3c4d4f60ea53874c31140e7aad0e1c0
Talha <mdtalha2008@gmail.com>
2024-10-02 20:15:16 +0600

    schedule data add

--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React from "react";
 
+import { Link } from "react-router-dom";
+// import { PopupButton } from "react-calendly";
 const Schedule = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Link to="/sched"></Link>
+      {/* <PopupButton
+                url="https://calendly.com/tariquetalha6/oceancapp"
+                
+                rootElement={document.getElementById("root")}
+                text="Click here to schedule!"
+                textColor="#ffffff"
+                color="#00a2ff
+            /> */}
+      <div >
+        <iframe
+          src="https://calendly.com/oceancapp/30-minute-meeting"
+          width="100%"
+          height="600"
+          // eslint-disable-next-line react/no-unknown-property
+          frameborder="0"
+          id="calendlyFrame"
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default Schedule;
