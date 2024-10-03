fabb03b472d76c024416306612d6833725111b86
Talha <mdtalha2008@gmail.com>
2024-10-03 22:15:22 +0600

    zego ui kit add

--- src/pages/VideoConferance/VideoConferance.jsx
+++ src/pages/VideoConferance/VideoConferance.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 const VideoConferance = () => {
     const {id}=useParams();
