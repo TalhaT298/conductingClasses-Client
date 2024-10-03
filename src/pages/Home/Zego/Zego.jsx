45988456ca8e12b83c00cf457431c2952efee056
Talha <mdtalha2008@gmail.com>
2024-10-03 21:00:42 +0600

    zego cloud button add

--- src/pages/Home/Zego/Zego.jsx
+++ src/pages/Home/Zego/Zego.jsx
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Zego = () => {
-    const [input,setInput]=useState("");
-    return (
-        <div>
-            <div>
-                
-            </div>
-        </div>
-    );
+  const [input, setInput] = useState("");
+  return (
+    <div>
+      <div>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          placeholder="Enter ypur name"
+        />
+        <button>Join</button>
+      </div>
+    </div>
+  );
 };
 
 export default Zego;
