d09c26d4fbc1d3b66141c13323ec3be90569b427
Talha <mdtalha2008@gmail.com>
2024-10-03 22:40:58 +0600

    video conferance add

--- src/pages/VideoConferance/VideoConferance.jsx
+++ src/pages/VideoConferance/VideoConferance.jsx
@@ -1,45 +1,64 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import React, { useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+// Function to generate a random ID of a given length
+const randomID = (length) => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 
 const VideoConferance = () => {
-    const {id}=useParams();
-    export default function App() {
-        const roomID = getUrlParams().get('roomID') || randomID(5);
-        let myMeeting = async (element) => {
-       // generate Kit Token
-        const appID = ;
-        const serverSecret = "";
-        const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
-  
-      
-       // Create instance object from Kit Token.
-        const zp = ZegoUIKitPrebuilt.create(kitToken);
-        // start the call
-        zp.joinRoom({
-          container: element,
-          sharedLinks: [
-            {
-              name: 'Personal link',
-              url:
-               window.location.protocol + '//' + 
-               window.location.host + window.location.pathname +
-                '?roomID=' +
-                roomID,
-            },
-          ],
-          scenario: {
-            mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+  const { id } = useParams();
+  const roomID = id || new URLSearchParams(window.location.search).get("roomID") || randomID(5);
+  const meetingRef = useRef(null);  // Use ref to get the div for the video conference
+
+  useEffect(() => {
+    const myMeeting = async (element) => {
+      // Generate Kit Token
+      const appID = 1736923256;
+      const serverSecret = "4e9a9f789445dc7aec24a10d2e2c72d6";
+      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+        appID,
+        serverSecret,
+        roomID,
+        Date.now().toString(),
+        "Talha"
+      );
+
+      // Create instance object from Kit Token
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+      // Start the call
+      zp.joinRoom({
+        container: element,
+        sharedLinks: [
+          {
+            name: "Copy link",
+            url: window.location.href,
           },
-        });
-  
-      
+        ],
+      });
     };
-    return (
-        <div className='py-20'>
-            <div>videopage {id}</div>
-        </div>
-    );
+
+    // Initialize the meeting when the component is mounted
+    if (meetingRef.current) {
+      myMeeting(meetingRef.current);
+    }
+  }, [roomID]);
+
+  return (
+    <div>
+      <h1>Video Conference</h1>
+      {/* Div that will contain the video conference */}
+      <div ref={meetingRef} style={{ width: '100%', height: '100vh' }}></div>
+    </div>
+  );
 };
 
 export default VideoConferance;
