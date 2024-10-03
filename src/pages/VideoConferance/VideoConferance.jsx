20361fe98c06100d7474bb6bcecc52e2fbf74b99
Talha <mdtalha2008@gmail.com>
2024-10-03 22:17:33 +0600

    video conversence add

--- src/pages/VideoConferance/VideoConferance.jsx
+++ src/pages/VideoConferance/VideoConferance.jsx
@@ -4,6 +4,37 @@ import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 const VideoConferance = () => {
     const {id}=useParams();
+    export default function App() {
+        const roomID = getUrlParams().get('roomID') || randomID(5);
+        let myMeeting = async (element) => {
+       // generate Kit Token
+        const appID = ;
+        const serverSecret = "";
+        const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
+  
+      
+       // Create instance object from Kit Token.
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
+        // start the call
+        zp.joinRoom({
+          container: element,
+          sharedLinks: [
+            {
+              name: 'Personal link',
+              url:
+               window.location.protocol + '//' + 
+               window.location.host + window.location.pathname +
+                '?roomID=' +
+                roomID,
+            },
+          ],
+          scenario: {
+            mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+          },
+        });
+  
+      
+    };
     return (
         <div className='py-20'>
             <div>videopage {id}</div>
